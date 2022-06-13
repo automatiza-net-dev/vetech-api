d851c667bb04738ae489ad9623ac930451d0dcef
STNeto1 <netofreitas7052@gmail.com>
2022-06-13 13:10:49 -0300

    feat: initial user model and migration

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,16 +1,51 @@
-import { DateTime } from "luxon";
-import Hash from "@ioc:Adonis/Core/Hash";
-import { column, beforeSave, BaseModel } from "@ioc:Adonis/Lucid/Orm";
+import Hash from '@ioc:Adonis/Core/Hash';
+import { BaseModel, beforeSave, column } from '@ioc:Adonis/Lucid/Orm';
+import { DateTime } from 'luxon';
+import { v4 } from 'uuid';
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
-  public id: number;
+  public id: string;
 
   @column()
   public email: string;
 
   @column({ serializeAs: null })
   public password: string;
+
+  @column()
+  public document: string;
+
+  @column()
+  public phone?: string;
+
+  @column({
+    columnName: 'profile_picture',
+  })
+  public profilePicture?: string;
+
+  @column({
+    columnName: 'postal_code',
+  })
+  public postalCode?: string; // CEP
+
+  @column()
+  public address?: string; // Endereço
+
+  @column()
+  public number?: string; // Número
+
+  @column()
+  public complement?: string; // Complemento
+
+  @column()
+  public district?: string; // Bairro
+
+  @column()
+  public city?: string; // Cidade
+
+  @column()
+  public state?: string; // Estado
 
   @column()
   public rememberMeToken?: string;
@@ -25,6 +60,13 @@ export default class User extends BaseModel {
   public static async hashPassword(user: User) {
     if (user.$dirty.password) {
       user.password = await Hash.make(user.password);
+    }
+  }
+
+  @beforeSave()
+  public static async checkId(user: User) {
+    if (!user.id) {
+      user.id = v4();
     }
   }
 }
--- database/migrations/1654537826669_users.ts
+++ database/migrations/1654537826669_users.ts
@@ -1,24 +1,34 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import BaseSchema from '@ioc:Adonis/Lucid/Schema';
 
 export default class UsersSchema extends BaseSchema {
-  protected tableName = 'users'
+  protected tableName = 'users';
 
   public async up() {
-    this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
-      table.string('email', 255).notNullable()
-      table.string('password', 180).notNullable()
-      table.string('remember_me_token').nullable()
+    this.schema.createTable(this.tableName, table => {
+      table.uuid('id').primary();
+      table.string('email', 255).notNullable();
+      table.string('password', 180).notNullable();
+      table.string('document', 20).notNullable();
+      table.string('phone', 20).nullable();
+      table.string('profile_picture', 255).nullable();
+      table.string('postal_code', 255).nullable();
+      table.string('address', 255).nullable();
+      table.string('number', 255).nullable();
+      table.string('complement', 255).nullable();
+      table.string('district', 255).nullable();
+      table.string('city', 255).nullable();
+      table.string('state', 255).nullable();
+      table.string('remember_me_token').nullable();
 
       /**
        * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true }).notNullable()
-      table.timestamp('updated_at', { useTz: true }).notNullable()
-    })
+      table.timestamp('created_at', { useTz: true }).notNullable();
+      table.timestamp('updated_at', { useTz: true }).notNullable();
+    });
   }
 
   public async down() {
-    this.schema.dropTable(this.tableName)
+    this.schema.dropTable(this.tableName);
   }
 }

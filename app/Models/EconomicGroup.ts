873d1beb37be66a1e61acd6d341514601312ae67
STNeto1 <netofreitas7052@gmail.com>
2022-06-14 16:03:42 -0300

    feat: database related features

--- app/Models/EconomicGroup.ts
+++ app/Models/EconomicGroup.ts
@@ -1,13 +1,36 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm';
+import { DateTime } from 'luxon';
 
 export default class EconomicGroup extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: string;
+
+  @column({
+    columnName: 'fantasy_name',
+  })
+  public fantasyName: string;
+
+  @column({
+    columnName: 'company_name',
+  })
+  public companyName: string;
+
+  @column({})
+  public document: string;
+
+  @column({
+    columnName: 'responsible_email',
+  })
+  public responsibleEmail: string;
+
+  @column({
+    columnName: 'responsible_phone',
+  })
+  public responsiblePhone: string;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime;
 }
--- database/migrations/1655231954176_economic_groups.ts
+++ database/migrations/1655231954176_economic_groups.ts
@@ -5,13 +5,20 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, table => {
-      table.increments('id');
+      table.uuid('id').primary();
+
+      table.string('fantasy_name', 255);
+      table.string('company_name', 255);
+      table.string('document', 20);
+      table.string('responsible_email', 120);
+      table.string('responsible_phone', 30);
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
        */
       table.timestamp('created_at', { useTz: true });
       table.timestamp('updated_at', { useTz: true });
+      table.dateTime('deleted_at').defaultTo(null);
     });
   }
 

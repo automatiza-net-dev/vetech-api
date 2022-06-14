933e47344616165a3eb62b0693be9e2efee48a10
STNeto1 <netofreitas7052@gmail.com>
2022-06-14 11:54:19 -0300

    test: forgot/reset password

--- tests/functional/user_resource.spec.ts
+++ tests/functional/user_resource.spec.ts
@@ -1,3 +1,4 @@
+import Encryption from '@ioc:Adonis/Core/Encryption';
 import Database from '@ioc:Adonis/Lucid/Database';
 import { test } from '@japa/runner';
 import User from 'App/Models/User';
@@ -70,5 +71,47 @@ test.group('User resource', group => {
     const getResponse = await client.get(`/users/${user.id}`);
     const body = getResponse.body();
     assert.equal('E_NOT_FOUND: The user was not found', body.message as string);
+  });
+
+  test('forgot password', async ({ client, assert }) => {
+    const [user] = await User.all();
+
+    const response = await client.post(`/users/forgot-password`).json({
+      email: user.email,
+    });
+    assert.equal(204, response.status());
+    // TODO assert email was sent
+  });
+
+  test('throw error on invalid hash', async ({ client, assert }) => {
+    const [user] = await User.all();
+    const hash = Encryption.encrypt('invalid@mail.com', '30min');
+
+    const response = await client.post(`/users/reset-password`).json({
+      email: user.email,
+      password: '102030',
+      password_confirmation: '102030',
+      hash,
+    });
+
+    assert.equal(400, response.status());
+    assert.equal(
+      'E_UNAUTHORIZED: Token inválido',
+      response.body().message as string,
+    );
+  });
+
+  test('reset password', async ({ client, assert }) => {
+    const [user] = await User.all();
+    const hash = Encryption.encrypt(user.email, '30min');
+
+    const response = await client.post(`/users/reset-password`).json({
+      email: user.email,
+      password: '102030',
+      password_confirmation: '102030',
+      hash,
+    });
+
+    assert.equal(204, response.status());
   });
 });

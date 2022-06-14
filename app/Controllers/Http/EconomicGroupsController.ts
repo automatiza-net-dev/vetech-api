117a5dd7aaa9af71107b6d0720612c9b639dfe9b
STNeto1 <netofreitas7052@gmail.com>
2022-06-14 16:59:35 -0300

    feat: update economic group

--- app/Controllers/Http/EconomicGroupsController.ts
+++ app/Controllers/Http/EconomicGroupsController.ts
@@ -1,6 +1,7 @@
 import { inject } from '@adonisjs/fold';
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import EconomicGroupService from 'App/Services/EconomicGroupService';
+import UpdateEconomicGroupValidator from 'App/Validators/EconomicGroup/UpdateEconomicGroupValidator';
 
 @inject()
 export default class EconomicGroupsController {
@@ -8,5 +9,13 @@ export default class EconomicGroupsController {
 
   public async index({ response }: HttpContextContract) {
     return response.ok(await this.service.index());
+  }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    const { id } = params;
+    const payload = await request.validate(UpdateEconomicGroupValidator);
+    const group = await this.service.update(id, payload);
+
+    return response.ok(group);
   }
 }
--- app/Services/EconomicGroupService.ts
+++ app/Services/EconomicGroupService.ts
@@ -1,9 +1,34 @@
 import { inject } from '@adonisjs/fold';
+import ResourceNotFoundException from 'App/Exceptions/ResourceNotFoundException';
 import EconomicGroup from 'App/Models/EconomicGroup';
+import { IUpdateEconomicGroup } from 'Contracts/interfaces/UpdateEconomicGroup';
 
 @inject()
 export default class EconomicGroupService {
   public async index(): Promise<Array<EconomicGroup>> {
     return EconomicGroup.all();
+  }
+
+  public async show(id: string): Promise<EconomicGroup> {
+    const group = await EconomicGroup.find(id);
+
+    if (!group) {
+      throw new ResourceNotFoundException(
+        'O grupo econômico não foi encontrado',
+        404,
+        'E_NOT_FOUND',
+      );
+    }
+
+    return group;
+  }
+
+  public async update(
+    id: string,
+    data: IUpdateEconomicGroup,
+  ): Promise<EconomicGroup> {
+    const group = await this.show(id);
+
+    return group.merge(data).save();
   }
 }

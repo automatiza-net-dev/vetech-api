7128cb8cb238e89eba57493caab62720a91b16d2
STNeto1 <netofreitas7052@gmail.com>
2022-06-15 10:03:13 -0300

    feat: resource bootstrap

---
+++ database/factories/BusinessUnitFactory.ts
@@ -0,0 +1,8 @@
+import Factory from '@ioc:Adonis/Lucid/Factory';
+import BusinessUnit from 'App/Models/BusinessUnit';
+
+export default Factory.define(BusinessUnit, ({ faker }) => {
+  return {
+    //
+  };
+}).build();

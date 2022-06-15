359404e4ed1ff92679b08d65ec9da97d91064dce
STNeto1 <netofreitas7052@gmail.com>
2022-06-15 12:10:27 -0300

    feat: role resource starter

---
+++ database/seeders/Role.ts
@@ -0,0 +1,21 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
+import Role from 'App/Models/Role';
+
+export default class extends BaseSeeder {
+  protected BASE_ROLES = [
+    'admin',
+    'manager',
+    'veterinary',
+    'attendant',
+    'clerk',
+    'seller',
+  ];
+
+  public async run() {
+    const partialRoles = this.BASE_ROLES.map(role => ({
+      name: role,
+    }));
+    await Role.createMany(partialRoles);
+    // Write your database queries inside the run method
+  }
+}

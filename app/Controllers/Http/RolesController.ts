cdc768c350ef58e68bdd0f6279856b1c031909b2
STNeto1 <netofreitas7052@gmail.com>
2022-06-15 12:36:20 -0300

    feat: create role

--- app/Controllers/Http/RolesController.ts
+++ app/Controllers/Http/RolesController.ts
@@ -1,6 +1,7 @@
 import { inject } from '@adonisjs/fold';
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import RoleService from 'App/Services/RoleService';
+import CreateRoleValidator from 'App/Validators/Role/CreateRoleValidator';
 
 @inject()
 export default class RolesController {
@@ -8,5 +9,12 @@ export default class RolesController {
 
   public async index({ response }: HttpContextContract) {
     response.ok(await this.roleService.index());
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    const payload = await request.validate(CreateRoleValidator);
+    const newRole = await this.roleService.store(payload);
+
+    return response.created(newRole);
   }
 }
--- app/Services/RoleService.ts
+++ app/Services/RoleService.ts
@@ -1,9 +1,14 @@
 import { inject } from '@adonisjs/fold';
 import Role from 'App/Models/Role';
+import ICreateRole from 'Contracts/interfaces/CreateRole';
 
 @inject()
 export default class RoleService {
   public async index(): Promise<Array<Role>> {
     return Role.all();
+  }
+
+  public async store(data: ICreateRole): Promise<Role> {
+    return Role.create(data);
   }
 }

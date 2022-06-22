1d211db70ddd3cdabaab77da823e158b444c69bf
STNeto1 <netofreitas7052@gmail.com>
2022-06-22 17:08:29 -0300

    fix: typescript augmentation

--- contracts/mail.ts
+++ contracts/mail.ts
@@ -1,14 +1,7 @@
-/**
- * Contract source: https://git.io/JvgAT
- *
- * Feel free to let us know via PR, if you find something broken in this contract
- * file.
- */
-
-import { InferMailersFromConfig } from '@adonisjs/mail/build/config';
-
-import mailConfig from '../config/mail';
-
 declare module '@ioc:Adonis/Addons/Mail' {
-  type MailersList = InferMailersFromConfig<typeof mailConfig>;
+  import { MailDrivers } from '@ioc:Adonis/Addons/Mail';
+
+  interface MailersList {
+    mailgun: MailDrivers['mailgun'];
+  }
 }

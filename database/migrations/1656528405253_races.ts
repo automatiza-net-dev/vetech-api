39fdc919f7c0269953e1a0e75a8c9ae0a99c798b
STNeto1 <netofreitas7052@gmail.com>
2022-06-29 16:29:33 -0300

    feat: race bootstrap

---
+++ database/migrations/1656528405253_races.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema';
+
+export default class extends BaseSchema {
+  protected tableName = 'races';
+
+  public async up() {
+    this.schema.createTable(this.tableName, table => {
+      table.uuid('id').primary();
+
+      table.string('description');
+      table.uuid('specie_id').references('species.id');
+
+      table.timestamp('created_at', { useTz: true });
+      table.timestamp('updated_at', { useTz: true });
+      table.dateTime('deleted_at').defaultTo(null);
+    });
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}

8fa6c9b8120aad9530da5cb0547a5f7b5c7e3ace
STNeto1 <netofreitas7052@gmail.com>
2022-07-05 18:03:29 -0300

    feat: schedule status operations

--- app/Controllers/Http/ScheduleStatusesController.ts
+++ app/Controllers/Http/ScheduleStatusesController.ts
@@ -2,10 +2,30 @@ import { inject } from '@adonisjs/fold';
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import ScheduleStatusService from 'App/Services/ScheduleStatusService';
 import CreateScheduleStatusValidator from 'App/Validators/ScheduleStatus/CreateScheduleStatusValidator';
+import UpdateScheduleStatusValidator from 'App/Validators/ScheduleStatus/UpdateScheduleStatusValidator';
 
 @inject()
 export default class ScheduleStatusesController {
   constructor(private readonly service: ScheduleStatusService) {}
+
+  public async index({ auth, response }: HttpContextContract) {
+    const result = await this.service.index(
+      auth.use('api').token!.meta.unit_id,
+      auth.use('api').user!,
+    );
+
+    return response.ok(result);
+  }
+
+  public async show({ auth, params, response }: HttpContextContract) {
+    const result = await this.service.show(
+      auth.use('api').token!.meta.unit_id,
+      auth.use('api').user!,
+      params.id,
+    );
+
+    return response.ok(result);
+  }
 
   public async store({ auth, request, response }: HttpContextContract) {
     const payload = await request.validate(CreateScheduleStatusValidator);
@@ -17,5 +37,33 @@ export default class ScheduleStatusesController {
     );
 
     return response.created(result);
+  }
+
+  public async update({
+    auth,
+    params,
+    request,
+    response,
+  }: HttpContextContract) {
+    const payload = await request.validate(UpdateScheduleStatusValidator);
+
+    const result = await this.service.update(
+      auth.use('api').token!.meta.unit_id,
+      auth.use('api').user!,
+      params.id,
+      payload,
+    );
+
+    return response.ok(result);
+  }
+
+  public async destroy({ auth, params, response }: HttpContextContract) {
+    await this.service.destroy(
+      auth.use('api').token!.meta.unit_id,
+      auth.use('api').user!,
+      params.id,
+    );
+
+    return response.noContent();
   }
 }
--- tests/functional/schedule_status_resource.spec.ts
+++ tests/functional/schedule_status_resource.spec.ts
@@ -39,7 +39,12 @@ test.group('Schedule status resource', group => {
       type: LicenceType.TRIAL,
     });
 
-    return { user, newBusinessUnit };
+    const status = await newGroup.related('scheduleStatuses').create({
+      color: 'color1',
+      description: 'description1',
+    });
+
+    return { user, newBusinessUnit, status };
   };
 
   const assignSuperAdmin = async (user: User, businessUnit: BusinessUnit) => {
@@ -95,5 +100,147 @@ test.group('Schedule status resource', group => {
 
     assert.equal(201, response.status());
     assert.isDefined(response.body().economic_group_id);
+  });
+
+  test('should return a list of schedule statuses', async ({
+    assert,
+    client,
+  }) => {
+    const { user, status } = await createData();
+
+    const token = await generateJwtToken(client, {
+      email: user.email,
+      password: '102030',
+    });
+
+    const response = await client.get('/schedule-statuses').bearerToken(token);
+
+    const body = response.body();
+
+    assert.equal(200, response.status());
+    assert.isArray(body);
+    assert.isTrue(Boolean(body.find(b => b.id === status.id)));
+  });
+
+  test('should throw ResourceNotFoundException if no status was found', async ({
+    assert,
+    client,
+  }) => {
+    const { user } = await createData();
+
+    const token = await generateJwtToken(client, {
+      email: user.email,
+      password: '102030',
+    });
+
+    const response = await client
+      .get(`/schedule-statuses/${v4()}`)
+      .bearerToken(token);
+
+    const body = response.body();
+
+    assert.equal(404, response.status());
+    assert.equal('E_NOT_FOUND: Recurso não foi encontrado', body.message);
+  });
+
+  test('should throw ResourceNotFoundException if status do not belong to user group', async ({
+    assert,
+    client,
+  }) => {
+    const { user } = await createData();
+    const { status } = await createData();
+
+    const token = await generateJwtToken(client, {
+      email: user.email,
+      password: '102030',
+    });
+
+    const response = await client
+      .get(`/schedule-statuses/${status.id}`)
+      .bearerToken(token);
+
+    const body = response.body();
+
+    assert.equal(404, response.status());
+    assert.equal('E_NOT_FOUND: Recurso não foi encontrado', body.message);
+  });
+
+  test('should return status for super admin', async ({ assert, client }) => {
+    const { user, newBusinessUnit } = await createData();
+    const { status } = await createData();
+
+    await assignSuperAdmin(user, newBusinessUnit);
+
+    const token = await generateJwtToken(client, {
+      email: user.email,
+      password: '102030',
+    });
+
+    const response = await client
+      .get(`/schedule-statuses/${status.id}`)
+      .bearerToken(token);
+
+    const body = response.body();
+
+    assert.equal(200, response.status());
+    assert.equal(status.id, body.id);
+  });
+
+  test('should return status for same group user', async ({
+    assert,
+    client,
+  }) => {
+    const { user, status } = await createData();
+
+    const token = await generateJwtToken(client, {
+      email: user.email,
+      password: '102030',
+    });
+
+    const response = await client
+      .get(`/schedule-statuses/${status.id}`)
+      .bearerToken(token);
+
+    const body = response.body();
+
+    assert.equal(200, response.status());
+    assert.equal(status.id, body.id);
+  });
+
+  test('should update a schedule status', async ({ assert, client }) => {
+    const { user, status } = await createData();
+
+    const token = await generateJwtToken(client, {
+      email: user.email,
+      password: '102030',
+    });
+
+    const response = await client
+      .put(`/schedule-statuses/${status.id}`)
+      .json({
+        description: 'new description',
+        color: '#000',
+      })
+      .bearerToken(token);
+
+    const body = response.body();
+
+    assert.equal(200, response.status());
+    assert.notEqual(status.description, body.description);
+  });
+
+  test('should soft delete a status', async ({ assert, client }) => {
+    const { user, status } = await createData();
+
+    const token = await generateJwtToken(client, {
+      email: user.email,
+      password: '102030',
+    });
+
+    const response = await client
+      .delete(`/schedule-statuses/${status.id}`)
+      .bearerToken(token);
+
+    assert.equal(204, response.status());
   });
 });

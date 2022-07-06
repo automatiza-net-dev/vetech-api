11bad31d82fd3da121c2d07e418f4dfcfeb707bd
STNeto1 <netofreitas7052@gmail.com>
2022-07-06 16:31:55 -0300

    feat: check disponibility

--- app/Services/ScheduleService.ts
+++ app/Services/ScheduleService.ts
@@ -1,4 +1,5 @@
 import { inject } from '@adonisjs/fold';
+import BadRequestException from 'App/Exceptions/BadRequestException';
 import Schedule from 'App/Models/Schedule';
 import User from 'App/Models/User';
 import IScheduleData from 'Contracts/interfaces/IScheduleData';
@@ -10,6 +11,42 @@ export default class ScheduleService {
     user: User,
     data: IScheduleData,
   ): Promise<Schedule> {
+    const exception = new BadRequestException(
+      'Usuário não tem esse horário disponível',
+      400,
+      'E_BAD_REQUEST',
+    );
+
+    const unavailableDays = await user
+      .related('unavailableDays')
+      .query()
+      .where('business_unit_id', unitId)
+      .andWhereRaw('start_hour <= ? or end_hour >= ?', [
+        data.startHour.toJSDate(),
+        data.endHour.toJSDate(),
+      ]);
+
+    if (unavailableDays.length !== 0) {
+      throw exception;
+    }
+
+    const overlapping = await Schedule.query()
+      .where('user_id', user.id)
+      .andWhere('business_unit_id', unitId)
+      .andWhereRaw('start_hour <= ? and end_hour >= ?', [
+        data.startHour.toJSDate(),
+        data.endHour.toJSDate(),
+      ])
+      .first();
+
+    if (overlapping) {
+      throw new BadRequestException(
+        'Horário já está ocupado',
+        400,
+        'E_BAD_REQUEST',
+      );
+    }
+
     return Schedule.create({
       patientName: data.patientName,
       patientPhone: data.patientPhone,

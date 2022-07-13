38a340bab221bd216ed777a8b7316dc3ead04a97
STNeto1 <netofreitas7052@gmail.com>
2022-07-13 09:11:16 -0300

    feat: variation migration, entity and relationships

--- app/Models/EconomicGroup.ts
+++ app/Models/EconomicGroup.ts
@@ -15,6 +15,7 @@ import ScheduleStatus from 'App/Models/ScheduleStatus';
 import Specie from 'App/Models/Specie';
 import UnavailableDay from 'App/Models/UnavailableDay';
 import User from 'App/Models/User';
+import Variation from 'App/Models/Variation';
 import WorkingDay from 'App/Models/WorkingDay';
 import { DateTime } from 'luxon';
 
@@ -107,4 +108,10 @@ export default class EconomicGroup extends BaseModel {
     foreignKey: 'economic_group_id',
   })
   public groups: HasMany<typeof Group>;
+
+  @hasMany(() => Variation, {
+    localKey: 'id',
+    foreignKey: 'economic_group_id',
+  })
+  public variations: HasMany<typeof Variation>;
 }

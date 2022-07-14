4d1a2be5235a3803c4941b412bf7c3f7b8e1f428
STNeto1 <netofreitas7052@gmail.com>
2022-07-14 11:19:50 -0300

    test: go back in test

--- tests/unit/variation_resource.spec.ts
+++ tests/unit/variation_resource.spec.ts
@@ -1,10 +1,11 @@
+import Database from '@ioc:Adonis/Lucid/Database';
 import { test } from '@japa/runner';
-import EconomicGroup from 'App/Models/EconomicGroup';
-import Variation from 'App/Models/Variation';
+import ResourceNotFoundException from 'App/Exceptions/ResourceNotFoundException';
 import SharedService from 'App/Services/SharedService';
 import VariationService from 'App/Services/VariationService';
-import sinon, { stubObject } from 'ts-sinon';
 import { v4 } from 'uuid';
+
+import { userBootstrap } from '../utils';
 
 test.group('Variation resource', group => {
   const sharedService = new SharedService();
@@ -12,73 +13,78 @@ test.group('Variation resource', group => {
 
   group.each.setup(async () => {
     service = new VariationService(sharedService);
+
+    await Database.beginGlobalTransaction();
+    return () => Database.rollbackGlobalTransaction();
   });
 
-  const ecoGroup = new EconomicGroup();
+  const createData = async () => {
+    const { user, group, business } = await userBootstrap();
 
-  test('should return all variations', async ({ assert }) => {
-    sinon
-      .stub(SharedService.prototype, 'getUserGroup')
-      .resolves(stubObject<EconomicGroup>(ecoGroup));
-    sinon.stub(Variation, 'query').returns({
-      where: () => [],
+    const variation = await group.related('variations').create({
+      description: 'some description',
     });
 
-    // const { unit, variation } = await createData();
+    return { user, unit: business, variation };
+  };
 
-    const result = await service.index(v4());
+  test('should return all variations', async ({ assert }) => {
+    const { unit, variation } = await createData();
+
+    const result = await service.index(unit.id);
 
     assert.isArray(result);
+    assert.isTrue(Boolean(result.find(f => f.id === variation.id)));
   });
 
-  // test('should throw ResourceNotFoundException if no variation was found', async ({
-  //   assert,
-  // }) => {
-  //   const { unit } = await createData();
-  //
-  //   try {
-  //     await service.show(unit.id, v4());
-  //   } catch (e) {
-  //     assert.instanceOf(e, ResourceNotFoundException);
-  //   }
-  // });
-  //
-  // test('should return variation', async ({ assert }) => {
-  //   const { unit, variation } = await createData();
-  //
-  //   const result = await service.show(unit.id, variation.id);
-  //
-  //   assert.equal(variation.id, result.id);
-  // });
-  //
-  // test('should store a new variation', async ({ assert }) => {
-  //   const { unit } = await createData();
-  //
-  //   const result = await service.store(unit.id, {
-  //     description: 'test description',
-  //   });
-  //
-  //   assert.equal('test description', result.description);
-  // });
-  //
-  // test('should update a variation', async ({ assert }) => {
-  //   const { unit, variation } = await createData();
-  //
-  //   const result = await service.update(unit.id, variation.id, {
-  //     description: 'new description',
-  //     active: true,
-  //   });
-  //
-  //   assert.notEqual(variation.description, result.description);
-  //   assert.equal('new description', result.description);
-  // });
-  //
-  // test('should soft delete a variation', async ({ assert }) => {
-  //   const { unit, variation } = await createData();
-  //
-  //   await service.destroy(unit.id, variation.id);
-  //
-  //   // tem o que testar?
-  //   assert.equal(1, 1);
-  // });
+  test('should throw ResourceNotFoundException if no variation was found', async ({
+    assert,
+  }) => {
+    const { unit } = await createData();
+
+    try {
+      await service.show(unit.id, v4());
+    } catch (e) {
+      assert.instanceOf(e, ResourceNotFoundException);
+    }
+  });
+
+  test('should return variation', async ({ assert }) => {
+    const { unit, variation } = await createData();
+
+    const result = await service.show(unit.id, variation.id);
+
+    assert.equal(variation.id, result.id);
+  });
+
+  test('should store a new variation', async ({ assert }) => {
+    const { unit } = await createData();
+
+    const result = await service.store(unit.id, {
+      description: 'test description',
+    });
+
+    assert.equal('test description', result.description);
+  });
+
+  test('should update a variation', async ({ assert }) => {
+    const { unit, variation } = await createData();
+
+    const result = await service.update(unit.id, variation.id, {
+      description: 'new description',
+      active: true,
+    });
+
+    assert.notEqual(variation.description, result.description);
+    assert.equal('new description', result.description);
+  });
+
+  test('should soft delete a variation', async ({ assert }) => {
+    const { unit, variation } = await createData();
+
+    await service.destroy(unit.id, variation.id);
+
+    // tem o que testar?
+    assert.equal(1, 1);
+  });
 });

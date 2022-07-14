c63ada857d0dcc9f587a7561533cba9afe3dd5ed
STNeto1 <netofreitas7052@gmail.com>
2022-07-14 17:16:01 -0300

    test: attach variation to group

--- tests/unit/variation_group_resource.spec.ts
+++ tests/unit/variation_group_resource.spec.ts
@@ -3,16 +3,18 @@ import { test } from '@japa/runner';
 import ResourceNotFoundException from 'App/Exceptions/ResourceNotFoundException';
 import SharedService from 'App/Services/SharedService';
 import VariationGroupService from 'App/Services/VariationGroupService';
+import VariationService from 'App/Services/VariationService';
 import { v4 } from 'uuid';
 
 import { userBootstrap } from '../utils';
 
 test.group('Variation group resource', group => {
   const sharedService = new SharedService();
+  const variationService = new VariationService(sharedService);
   let service: VariationGroupService;
 
   group.each.setup(async () => {
-    service = new VariationGroupService(sharedService);
+    service = new VariationGroupService(sharedService, variationService);
 
     await Database.beginGlobalTransaction();
     return () => Database.rollbackGlobalTransaction();
@@ -87,6 +89,27 @@ test.group('Variation group resource', group => {
     const { unit, variationGroup } = await createData();
 
     await service.destroy(unit.id, variationGroup.id);
+
+    // tem o que testar?
+    assert.equal(1, 1);
+  });
+
+  test('should attach a variation', async ({ assert }) => {
+    const { unit, variationGroup, variation } = await createData();
+
+    await service.assignVariation(unit.id, {
+      group_variation_id: variationGroup.id,
+      variation_id: variation.id,
+    });
+
+    // tem o que testar?
+    assert.equal(1, 1);
+  });
+
+  test('should detach a variation from a group', async ({ assert }) => {
+    const { unit, variationGroup, variation } = await createData();
+
+    await service.detach(unit.id, variationGroup.id, variation.id);
 
     // tem o que testar?
     assert.equal(1, 1);

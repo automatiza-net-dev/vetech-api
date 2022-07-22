3ea39440f9d54c726f4460b0f5490247562b7d89
STNeto1 <netofreitas7052@gmail.com>
2022-07-22 15:49:27 -0300

    fix: add specific uuids for timeline type

--- app/Models/TimelineType.ts
+++ app/Models/TimelineType.ts
@@ -8,6 +8,10 @@ import { softDelete, softDeleteQuery } from 'App/Services/SoftDelete';
 import { DateTime } from 'luxon';
 import { v4 } from 'uuid';
 
+export const PATHOLOGY_UUID = '5f6b40d0-5976-4fee-9162-91c7bbb750fb';
+export const DOCUMENT_UUID = '745a4fb1-f0b4-43ee-a97e-377faeefa84f';
+export const RECIPE_UUID = '15a959c1-c509-4a11-9a98-4ea54e12398d';
+
 export default class TimelineType extends BaseModel {
   @column({ isPrimary: true })
   public id: string = v4();
--- database/seeders/TimelineSeeder.ts
+++ database/seeders/TimelineSeeder.ts
@@ -1,19 +1,26 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
-import TimelineType from 'App/Models/TimelineType';
+import TimelineType, {
+  DOCUMENT_UUID,
+  PATHOLOGY_UUID,
+  RECIPE_UUID,
+} from 'App/Models/TimelineType';
 
 export default class extends BaseSeeder {
   private BASE: Array<Partial<TimelineType>> = [
     {
+      id: PATHOLOGY_UUID,
       description: 'Patologia',
       color: '#000',
       requiresObservation: false,
     },
     {
+      id: DOCUMENT_UUID,
       description: 'Documento',
       color: '#000',
       requiresObservation: false,
     },
     {
+      id: RECIPE_UUID,
       description: 'Formato Receita Médica',
       color: '#000',
       requiresObservation: false,
@@ -21,6 +28,6 @@ export default class extends BaseSeeder {
   ];
 
   public async run() {
-    await TimelineType.fetchOrCreateMany('description', this.BASE);
+    await TimelineType.fetchOrCreateMany('id', this.BASE);
   }
 }

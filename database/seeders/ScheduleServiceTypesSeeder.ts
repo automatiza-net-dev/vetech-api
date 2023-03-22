638d914e3500e134212f4b41b361cf0566236a3e
STNeto1 <gfreitasneto18@gmail.com>
2023-03-22 16:43:28 -0300

    fix: seeder type

--- database/seeders/ScheduleServiceTypesSeeder.ts
+++ database/seeders/ScheduleServiceTypesSeeder.ts
@@ -7,10 +7,12 @@ export default class extends BaseSeeder {
     {
       description: 'Avaliação Cirúrgica',
       allowReturn: true,
+      reservedMinutes: 45,
     },
     {
       description: 'Cirurgia',
       allowReturn: true,
+      reservedMinutes: 120,
     },
   ];
 
@@ -18,46 +20,57 @@ export default class extends BaseSeeder {
     {
       description: 'Consulta Dermatológica',
       allowReturn: true,
+      reservedMinutes: 45,
     },
     {
       description: 'Consulta Emergência Rotina',
       allowReturn: true,
+      reservedMinutes: 45,
     },
     {
       description: 'Consulta Endocrinológica',
       allowReturn: true,
+      reservedMinutes: 45,
     },
     {
       description: 'Consulta Neurológica ( Dra Larissa)',
       allowReturn: true,
+      reservedMinutes: 45,
     },
     {
       description: 'Consulta Odontológica',
       allowReturn: true,
+      reservedMinutes: 45,
     },
     {
       description: 'Consulta Oftalmológica',
       allowReturn: true,
+      reservedMinutes: 45,
     },
     {
       description: 'Consulta Ortopédica',
       allowReturn: true,
+      reservedMinutes: 45,
     },
     {
       description: 'Consulta Plantão',
       allowReturn: true,
+      reservedMinutes: 30,
     },
     {
       description: 'Consulta Rotina',
       allowReturn: true,
+      reservedMinutes: 45,
     },
     {
       description: 'Consulta Rotina Felinos',
       allowReturn: true,
+      reservedMinutes: 45,
     },
     {
       description: 'Histórico do Sisvet',
       allowReturn: false,
+      reservedMinutes: 45,
     },
   ];
 
@@ -65,30 +78,37 @@ export default class extends BaseSeeder {
     {
       description: 'Coleta de sangue',
       allowReturn: false,
+      reservedMinutes: 30,
     },
     {
       description: 'Coleta de sangue para transfusão',
       allowReturn: false,
+      reservedMinutes: 60,
     },
     {
       description: 'Coleta de urina guiada por Usg',
       allowReturn: false,
+      reservedMinutes: 60,
     },
     {
       description: 'Endoscopia/Colonoscopia',
       allowReturn: false,
+      reservedMinutes: 90,
     },
     {
       description: 'Raio-x externo',
       allowReturn: false,
+      reservedMinutes: 45,
     },
     {
       description: 'Ultrassom',
       allowReturn: false,
+      reservedMinutes: 30,
     },
     {
       description: 'Ultrassom Gestacional',
       allowReturn: false,
+      reservedMinutes: 45,
     },
   ];
 
@@ -96,18 +116,22 @@ export default class extends BaseSeeder {
     {
       description: 'Alta',
       allowReturn: false,
+      reservedMinutes: 30,
     },
     {
       description: 'Encaminhamento - Receptivo',
       allowReturn: false,
+      reservedMinutes: 20,
     },
     {
       description: 'Internamento',
       allowReturn: false,
+      reservedMinutes: 30,
     },
     {
       description: 'Visita',
       allowReturn: false,
+      reservedMinutes: 60,
     },
   ];
 
@@ -115,18 +139,22 @@ export default class extends BaseSeeder {
     {
       description: 'Curativo pós cirúrgico',
       allowReturn: false,
+      reservedMinutes: 30,
     },
     {
       description: 'Procedimento',
       allowReturn: false,
+      reservedMinutes: 30,
     },
     {
       description: 'Retirada de pontos',
       allowReturn: false,
+      reservedMinutes: 30,
     },
     {
       description: 'Vacinação',
       allowReturn: false,
+      reservedMinutes: 30,
     },
   ];
 
@@ -134,14 +162,17 @@ export default class extends BaseSeeder {
     {
       description: 'Reavaliação',
       allowReturn: false,
+      reservedMinutes: 45,
     },
     {
       description: 'Relatório de raio-x',
       allowReturn: false,
+      reservedMinutes: 45,
     },
     {
       description: 'Retorno',
       allowReturn: false,
+      reservedMinutes: 45,
     },
   ];
 

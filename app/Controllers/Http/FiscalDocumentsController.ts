0a3f37c3430a0ec6a70df6f41d644f1a84230941
STNeto1 <gfreitasneto18@gmail.com>
2023-03-24 09:40:46 -0300

    feat: fiscal document search

--- app/Controllers/Http/FiscalDocumentsController.ts
+++ app/Controllers/Http/FiscalDocumentsController.ts
@@ -1,14 +1,12 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { inject } from "@adonisjs/fold";
+import { inject } from '@adonisjs/fold';
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import FiscalDocumentService from 'App/Services/FiscalDocumentService';
 
 @inject()
 export default class FiscalDocumentsController {
-  constructor(private readonly service: FiscalDocumentService) {
-
-  }
-  async index({ response }: HttpContextContract) {
-    const result = await this.service.index();
+  constructor(private readonly service: FiscalDocumentService) {}
+  async index({ request, response }: HttpContextContract) {
+    const result = await this.service.index(request.qs());
 
     return response.ok(result);
   }
--- app/Services/FiscalDocumentService.ts
+++ app/Services/FiscalDocumentService.ts
@@ -1,9 +1,24 @@
-import { inject } from "@adonisjs/fold";
-import FiscalDocument from "App/Models/FiscalDocument";
+import { inject } from '@adonisjs/fold';
+import FiscalDocument from 'App/Models/FiscalDocument';
+
+type ISearch = {
+  document?: string;
+  movement?: string;
+};
 
 @inject()
 export default class FiscalDocumentService {
-  async index() {
-    return FiscalDocument.query().where('active', true)
+  async index(data: ISearch) {
+    const qb = FiscalDocument.query().where('active', true);
+
+    if (data.document) {
+      qb.where('document_type', data.document);
+    }
+
+    if (data.movement) {
+      qb.where('movement_type', data.movement);
+    }
+
+    return qb;
   }
 }

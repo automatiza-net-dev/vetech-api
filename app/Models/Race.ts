46e91db22995c51c304823139b348cdf61f38c1e
STNeto1 <gfreitasneto18@gmail.com>
2023-04-11 15:52:24 -0300

    feat: race system

--- app/Models/Race.ts
+++ app/Models/Race.ts
@@ -52,6 +52,11 @@ export default class Race extends BaseModel {
   @column({
     serializeAs: null,
   })
+  public system_id: number;
+
+  @column({
+    serializeAs: null,
+  })
   public economic_group_id?: string;
 
   @belongsTo(() => EconomicGroup, {})

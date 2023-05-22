e894697375c454601b6ed3f52ea635ad4f2f8da1
STNeto1 <gfreitasneto18@gmail.com>
2023-05-22 11:36:17 -0300

    fix: field

--- app/Services/ScheduleService.ts
+++ app/Services/ScheduleService.ts
@@ -46,7 +46,7 @@ interface IHomeSearch {
 
 @inject()
 export default class ScheduleService {
-  constructor(private readonly sharedService: SharedService) {}
+  constructor(private readonly sharedService: SharedService) { }
 
   public async homeContent(unitId: string, data: IHomeSearch) {
     const qb = Schedule.query()
@@ -437,16 +437,16 @@ export default class ScheduleService {
 
     const [wDays, uDays, schedules] = data.user
       ? await this.getUserGeneralSchedules(
-          data.user,
-          data.business,
-          startDate,
-          endDate,
-        )
+        data.user,
+        data.business,
+        startDate,
+        endDate,
+      )
       : await this.getGeneralSchedules(
-          data.business,
-          startOfDay(startDate),
-          endOfDay(endDate),
-        );
+        data.business,
+        startOfDay(startDate),
+        endOfDay(endDate),
+      );
 
     return this.mapSchedulesToDays(keys, wDays, uDays, schedules);
   }
@@ -641,7 +641,7 @@ export default class ScheduleService {
         name: patient?.name,
         photo: patient?.photo,
         tag: patient?.tag,
-        cellphone: patient?.tutor.cellphone,
+        cellphone: patient?.tutor?.cellphone ?? null,
       };
 
       return jsonKinda;
@@ -1079,14 +1079,14 @@ export default class ScheduleService {
       },
       cancellation: elem.cancellationUser
         ? {
-            technician: {
-              id: elem.user?.id,
-              name: elem.user?.name,
-            },
-            reason: elem.reason?.reason,
-            observation: elem.observation,
-            cancelledAt: elem.updatedAt,
-          }
+          technician: {
+            id: elem.user?.id,
+            name: elem.user?.name,
+          },
+          reason: elem.reason?.reason,
+          observation: elem.observation,
+          cancelledAt: elem.updatedAt,
+        }
         : null,
       reschedules: elem.reschedules.map(r => ({
         id: r.id,

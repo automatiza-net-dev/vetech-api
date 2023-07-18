9a3a2dbe20adf097edc707a33581c6295a9311ca
STNeto1 <gfreitasneto18@gmail.com>
2023-07-18 12:04:50 -0300

    fix: prefix

--- app/Services/BusinessUnitFiscalDocumentService.ts
+++ app/Services/BusinessUnitFiscalDocumentService.ts
@@ -1029,6 +1029,11 @@ export default class BusinessUnitFiscalDocumentService {
     console.log('document:', document.toJSON());
     console.log('focus payload', data);
 
+    const urlPrefix =
+      process.env.NODE_ENV === 'production'
+        ? 'https://api.focusnfe.com.br'
+        : 'https://homologacao.focusnfe.com.br';
+
     return document.merge({
       sefazStatus:
         document.sefazStatus === 'Inutilizado'
@@ -1042,9 +1047,9 @@ export default class BusinessUnitFiscalDocumentService {
           ].join(' - ')
         : data.mensagem_sefaz,
       accessKey: data.chave_nfe,
-      authorizationXmlPath: data.caminho_xml_nota_fiscal,
-      authorizationPdfPath: data.caminho_danfe,
-      cancellationXmlPath: data.caminho_xml_cancelamento,
+      authorizationXmlPath: [urlPrefix, data.caminho_xml_nota_fiscal].join(''),
+      authorizationPdfPath: [urlPrefix, data.caminho_danfe].join(''),
+      cancellationXmlPath: [urlPrefix, data.caminho_xml_cancelamento].join(''),
 
       authorizationReceipt: data.protocolo_nota_fiscal?.numero_protocolo,
       authorizationReceiptDate: data.protocolo_nota_fiscal?.data_recebimento
@@ -1065,6 +1070,11 @@ export default class BusinessUnitFiscalDocumentService {
     console.log('document:', document.toJSON());
     console.log('focus nfse payload', data);
 
+    const urlPrefix =
+      process.env.NODE_ENV === 'production'
+        ? 'https://api.focusnfe.com.br'
+        : 'https://homologacao.focusnfe.com.br';
+
     return document.merge({
       status: data.status,
       sequence: data.numero,
@@ -1078,9 +1088,9 @@ export default class BusinessUnitFiscalDocumentService {
         data.status === 'autorizado' ? DateTime.now() : undefined,
       cancellationDate:
         data.status === 'cancelado' ? DateTime.now() : undefined,
-      mirrorPath: data.url,
-      authorizationPdfPath: data.url_danfse,
-      authorizationXmlPath: data.caminho_xml_nota_fiscal,
+      mirrorPath: [urlPrefix, data.url].join(''),
+      authorizationPdfPath: [urlPrefix, data.url_danfse].join(''),
+      authorizationXmlPath: [urlPrefix, data.caminho_xml_nota_fiscal].join(''),
     });
   }
 

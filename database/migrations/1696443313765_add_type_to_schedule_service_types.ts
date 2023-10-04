0af091bdb9129a4b6eab3ad67091ac1a11c72881
STNeto1 <gfreitasneto18@gmail.com>
2023-10-04 15:25:16 -0300

    fix: default type

--- database/migrations/1696443313765_add_type_to_schedule_service_types.ts
+++ database/migrations/1696443313765_add_type_to_schedule_service_types.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.alterTable(this.tableName, table => {
-      table.string('type').defaultTo('a');
+      table.string('type').defaultTo('A');
     });
   }
 

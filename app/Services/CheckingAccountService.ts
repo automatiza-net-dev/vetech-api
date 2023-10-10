6bdc0f8704d2e95c6f47366e5aea47c6b4e933ac
STNeto1 <gfreitasneto18@gmail.com>
2023-10-10 08:12:14 -0300

    feat: load into

--- app/Services/CheckingAccountService.ts
+++ app/Services/CheckingAccountService.ts
@@ -58,6 +58,9 @@ export default class CheckingAccountService {
     const account = await CheckingAccount.query()
       .where('id', id)
       .where('economic_group_id', authCtx.group.id)
+      .preload('unit', query => {
+        query.select('id', 'identification');
+      })
       .first();
 
     if (!account) {

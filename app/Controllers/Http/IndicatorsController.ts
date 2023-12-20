942193675a8d0c0eec7536e9cfd296a7f5db809d
STNeto1 <gfreitasneto18@gmail.com>
2023-12-20 13:50:35 -0300

    feat: new indicator route

--- app/Controllers/Http/IndicatorsController.ts
+++ app/Controllers/Http/IndicatorsController.ts
@@ -1,355 +1,368 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { inject } from '@adonisjs/fold';
-import IndicatorService from 'App/Services/IndicatorService';
-import SharedService from 'App/Services/SharedService';
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { inject } from "@adonisjs/fold";
+import IndicatorService from "App/Services/IndicatorService";
+import SharedService from "App/Services/SharedService";
 
 @inject()
 export default class IndicatorsController {
-  constructor(
-    private readonly sharedService: SharedService,
-    private readonly service: IndicatorService,
-  ) {}
+	constructor(
+		private readonly sharedService: SharedService,
+		private readonly service: IndicatorService,
+	) {}
 
-  public async medianTicket({ auth, request, response }: HttpContextContract) {
-    const result = await this.service.medianTicket(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async medianTicket({ auth, request, response }: HttpContextContract) {
+		const result = await this.service.medianTicket(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async medianTicketByOrigin({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.medianTicketByOrigin(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async medianTicketByOrigin({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.medianTicketByOrigin(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async invoicingByProductType({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.invoicingByProductType(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async invoicingByProductType({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.invoicingByProductType(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async invoicingByPaymentMethod({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.invoicingByPaymentMethod(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async invoicingByProductTypeWithSubgroup({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.invoicingByProductTypeWithSubgroup(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async invoicingByNewClients({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.invoicingByNewClients(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async invoicingByPaymentMethod({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.invoicingByPaymentMethod(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async medianTicketConsolidated({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.medianTicketConsolidated(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async invoicingByNewClients({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.invoicingByNewClients(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async medianTicketByOriginConsolidated({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.medianTicketByOriginConsolidated(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async medianTicketConsolidated({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.medianTicketConsolidated(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async invoicingByProductTypeConsolidated({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.invoicingByProductTypeConsolidated(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async medianTicketByOriginConsolidated({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.medianTicketByOriginConsolidated(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async invoicingByPaymentMethodConsolidated({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.invoicingByPaymentMethodConsolidated(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async invoicingByProductTypeConsolidated({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.invoicingByProductTypeConsolidated(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async invoicingByNewClientsConsolidated({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.invoicingByNewClientsConsolidated(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async invoicingByPaymentMethodConsolidated({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.invoicingByPaymentMethodConsolidated(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async schedulingIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.schedulingIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async invoicingByNewClientsConsolidated({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.invoicingByNewClientsConsolidated(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async subgroupIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.subgroupIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async schedulingIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.schedulingIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async consolidatedSubgroupIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.consolidatedSubgroupIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async subgroupIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.subgroupIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async opportunitiesIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.opportunitiesIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async consolidatedSubgroupIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.consolidatedSubgroupIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async generalOpportunitiesIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.generalOpportunitiesIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async opportunitiesIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.opportunitiesIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async crmIndicators({ auth, request, response }: HttpContextContract) {
-    const result = await this.service.crmIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async generalOpportunitiesIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.generalOpportunitiesIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async unconfirmedBudgetsIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.unconfirmedBudgetsIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async crmIndicators({ auth, request, response }: HttpContextContract) {
+		const result = await this.service.crmIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async projectionIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.projectionIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async unconfirmedBudgetsIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.unconfirmedBudgetsIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async billingIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.billingIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async projectionIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.projectionIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async productTypeIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.productTypeIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async billingIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.billingIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async salesPerPeriodIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.salesPerPeriodIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async productTypeIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.productTypeIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async budgetIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.budgetsIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async salesPerPeriodIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.salesPerPeriodIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async marketingIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.marketingIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async budgetIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.budgetsIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async costOfAcquisitionIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.costOfAcquisitionIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async marketingIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.marketingIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async billPaymentFormatIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.billPaymentFormatIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async costOfAcquisitionIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.costOfAcquisitionIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async installmentAvgIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.installmentAvgIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async billPaymentFormatIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.billPaymentFormatIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
 
-  public async avgReceiptDeadlineIndicators({
-    auth,
-    request,
-    response,
-  }: HttpContextContract) {
-    const result = await this.service.avgReceiptDeadlineIndicators(
-      await this.sharedService.getAuthContext(auth),
-      request.qs(),
-    );
+	public async installmentAvgIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.installmentAvgIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
 
-    return response.ok(result);
-  }
+		return response.ok(result);
+	}
+
+	public async avgReceiptDeadlineIndicators({
+		auth,
+		request,
+		response,
+	}: HttpContextContract) {
+		const result = await this.service.avgReceiptDeadlineIndicators(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
+
+		return response.ok(result);
+	}
 }

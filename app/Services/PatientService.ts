34b87da46adf9ac712981613960a96454e57dc9e
STNeto1 <gfreitasneto18@gmail.com>
2024-01-10 15:28:17 -0300

    fix: add on create as well

--- app/Services/PatientService.ts
+++ app/Services/PatientService.ts
@@ -812,6 +812,16 @@ export default class PatientService {
 
 			await group.related("patients").attach([patient.id], trx);
 
+			await patient.related("contacts").create(
+				{
+					main: true,
+					contact: data.cellphone,
+					observation: "Contato principal",
+					type: "celular",
+				},
+				{ client: trx },
+			);
+
 			return patient;
 		});
 	}

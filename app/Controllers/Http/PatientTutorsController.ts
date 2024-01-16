355bd63baa0d88a89eff5141fba740ed2afeb8ec
STNeto1 <gfreitasneto18@gmail.com>
2024-01-16 15:47:13 -0300

    feat: new validators

--- app/Controllers/Http/PatientTutorsController.ts
+++ app/Controllers/Http/PatientTutorsController.ts
@@ -1,77 +1,106 @@
-import { inject } from '@adonisjs/fold';
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import PatientService from 'App/Services/PatientService';
-import SharedService from 'App/Services/SharedService';
-import AssignPatientTutorValidator from 'App/Validators/Patient/AssignPatientTutorValidator';
-import CreatePatientWithTutorValidator from 'App/Validators/Patient/CreatePatientWithTutorValidator';
-import UpdatePatientWithTutorValidator from 'App/Validators/Patient/UpdatePatientWithTutorValidator';
+import { inject } from "@adonisjs/fold";
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import PatientService from "App/Services/PatientService";
+import SharedService from "App/Services/SharedService";
+import AssignPatientTutorValidator from "App/Validators/Patient/AssignPatientTutorValidator";
+import CreateLiftOneTutorForGenericValidator from "App/Validators/Patient/CreateLiftOneTutorForGenericValidator";
+import CreateLiftOneTutorForRegisterValidator from "App/Validators/Patient/CreateLiftOneTutorForRegisterValidator";
+import CreatePatientWithTutorValidator from "App/Validators/Patient/CreatePatientWithTutorValidator";
+import CreateSanclaTutorForGenericValidator from "App/Validators/Patient/CreateSanclaTutorForGenericValidator";
+import CreateSanclaTutorForRegisterValidator from "App/Validators/Patient/CreateSanclaTutorForRegisterValidator";
+import UpdateLiftOneTutorForGenericValidator from "App/Validators/Patient/UpdateLiftOneTutorForGenericValidator";
+import UpdateLiftOneTutorForRegisterValidator from "App/Validators/Patient/UpdateLiftOneTutorForRegisterValidator";
+import UpdatePatientWithTutorValidator from "App/Validators/Patient/UpdatePatientWithTutorValidator";
+import UpdateSanclaTutorForGenericValidator from "App/Validators/Patient/UpdateSanclaTutorForGenericValidator";
+import UpdateSanclaTutorForRegisterValidator from "App/Validators/Patient/UpdateSanclaTutorForRegisterValidator";
 
 @inject()
 export default class PatientTutorsController {
-  constructor(
-    private readonly service: PatientService,
-    private readonly sharedService: SharedService,
-  ) {}
+	constructor(
+		private readonly service: PatientService,
+		private readonly sharedService: SharedService,
+	) {}
 
-  public async index({ auth, request, response }: HttpContextContract) {
-    const { unit_id } = this.sharedService.extractUser(auth);
-    const qs = request.qs();
-    const patients = await this.service.tutorsIndex(unit_id, {
-      name: qs.name,
-      document: qs.document,
-      patient: qs.patient,
-      phone: qs.phone,
-      race: qs.race,
-    });
+	public async index({ auth, request, response }: HttpContextContract) {
+		const { unit_id } = this.sharedService.extractUser(auth);
+		const qs = request.qs();
+		const patients = await this.service.tutorsIndex(unit_id, {
+			name: qs.name,
+			document: qs.document,
+			patient: qs.patient,
+			phone: qs.phone,
+			race: qs.race,
+		});
 
-    return response.ok(patients);
-  }
+		return response.ok(patients);
+	}
 
-  public async notRelated({ auth, params, response }: HttpContextContract) {
-    const { unit_id } = this.sharedService.extractUser(auth);
+	public async notRelated({ auth, params, response }: HttpContextContract) {
+		const { unit_id } = this.sharedService.extractUser(auth);
 
-    const patients = await this.service.tutorNonPatients(unit_id, params.id);
+		const patients = await this.service.tutorNonPatients(unit_id, params.id);
 
-    return response.ok(patients);
-  }
+		return response.ok(patients);
+	}
 
-  public async show({ auth, params, response }: HttpContextContract) {
-    const { unit_id } = this.sharedService.extractUser(auth);
+	public async show({ auth, params, response }: HttpContextContract) {
+		const { unit_id } = this.sharedService.extractUser(auth);
 
-    const patients = await this.service.show(unit_id, params.id);
+		const patients = await this.service.show(unit_id, params.id);
 
-    return response.ok(patients);
-  }
+		return response.ok(patients);
+	}
 
-  public async store({ auth, request, response }: HttpContextContract) {
-    const payload = await request.validate(CreatePatientWithTutorValidator);
-    const { unit_id } = this.sharedService.extractUser(auth);
+	public async store({ auth, request, response }: HttpContextContract) {
+		const authCtx = await this.sharedService.getAuthContext(auth);
 
-    const patient = await this.service.storeTutor(unit_id, payload);
+		const origin = request.input("origin");
+		const payload = ["", "Cadastro"].includes(origin)
+			? authCtx.system.name === "Sanclá"
+				? await request.validate(CreateSanclaTutorForRegisterValidator)
+				: await request.validate(CreateLiftOneTutorForRegisterValidator)
+			: authCtx.system.name === "Sanclá"
+			  ? await request.validate(CreateSanclaTutorForGenericValidator)
+			  : await request.validate(CreateLiftOneTutorForGenericValidator);
+		await request.validate(CreatePatientWithTutorValidator);
 
-    return response.created(patient);
-  }
+		const { unit_id } = this.sharedService.extractUser(auth);
 
-  public async assign({ auth, request, response }: HttpContextContract) {
-    const payload = await request.validate(AssignPatientTutorValidator);
-    const { unit_id } = this.sharedService.extractUser(auth);
+		const patient = await this.service.storeTutor(unit_id, payload);
 
-    await this.service.assignPatientTutor(unit_id, payload);
+		return response.created(patient);
+	}
 
-    return response.created();
-  }
+	public async assign({ auth, request, response }: HttpContextContract) {
+		const payload = await request.validate(AssignPatientTutorValidator);
+		const { unit_id } = this.sharedService.extractUser(auth);
 
-  public async update({
-    auth,
-    params,
-    request,
-    response,
-  }: HttpContextContract) {
-    const payload = await request.validate(UpdatePatientWithTutorValidator);
-    const { unit_id } = this.sharedService.extractUser(auth);
+		await this.service.assignPatientTutor(unit_id, payload);
 
-    const patient = await this.service.updateTutor(unit_id, params.id, payload);
+		return response.created();
+	}
 
-    return response.ok(patient);
-  }
+	public async update({
+		auth,
+		params,
+		request,
+		response,
+	}: HttpContextContract) {
+		const authCtx = await this.sharedService.getAuthContext(auth);
+		const { unit_id } = this.sharedService.extractUser(auth);
+
+		const origin = request.input("origin");
+		const payload = ["", "Cadastro"].includes(origin)
+			? authCtx.system.name === "Sanclá"
+				? await request.validate(UpdateSanclaTutorForRegisterValidator)
+				: await request.validate(UpdateLiftOneTutorForRegisterValidator)
+			: authCtx.system.name === "Sanclá"
+			  ? await request.validate(UpdateSanclaTutorForGenericValidator)
+			  : await request.validate(UpdateLiftOneTutorForGenericValidator);
+		await request.validate(UpdatePatientWithTutorValidator);
+
+		const patient = await this.service.updateTutor(unit_id, params.id, payload);
+
+		return response.ok(patient);
+	}
 }

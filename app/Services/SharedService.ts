293eda8f0b86058a2ba955ed15f8e4573e6d34c9
STNeto1 <gfreitasneto18@gmail.com>
2024-01-23 15:19:16 -0300

    feat: refactor document checking

--- app/Services/SharedService.ts
+++ app/Services/SharedService.ts
@@ -118,11 +118,61 @@ export default class SharedService {
 	}
 
 	public validDocument(document: string): boolean {
-		const re =
-			/([0-9]{2}[.]?[0-9]{3}[.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[.]?[0-9]{3}[.]?[0-9]{3}[-]?[0-9]{2})/;
+		const filteredCPF = document.replace(/\.|-/g, "");
 
-		return re.test(document);
+		if (filteredCPF.length !== 11) {
+			return false;
+		}
+
+		const arrCPF: number[] = Array.from(filteredCPF, Number);
+
+		const repeatedNumbers: boolean = arrCPF.every(
+			(num, _, arr) => num === arr[0],
+		);
+		if (repeatedNumbers) {
+			return false;
+		}
+
+		const firstDigit = this.validateDigit(arrCPF, 1);
+		const secondDigit = this.validateDigit(arrCPF, 2);
+		if (!firstDigit || !secondDigit) {
+			return false;
+		}
+
+		return true;
 	}
+
+	private validateDigit = (arr: number[], position: number): boolean => {
+		let factor: number;
+		let arrayDigit: number;
+		let sum = 0;
+
+		if (position === 1) {
+			factor = 10;
+			arrayDigit = 9;
+		} else {
+			factor = 11;
+			arrayDigit = 10;
+		}
+
+		for (let i = 0; i < arrayDigit; i += 1) {
+			sum += arr[i] * factor;
+			factor -= 1;
+		}
+
+		const division = Math.floor(sum % 11);
+		let verifyingDigit = 0;
+
+		if (division > 1) {
+			verifyingDigit = 11 - division;
+		}
+
+		if (arr[arrayDigit] !== verifyingDigit) {
+			return false;
+		}
+
+		return true;
+	};
 
 	public isValidNumber(data: number | undefined) {
 		if (!data) {

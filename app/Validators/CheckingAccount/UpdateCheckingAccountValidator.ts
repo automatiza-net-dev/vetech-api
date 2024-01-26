521267c7fb2dbdee93a7cd4513de0cc4d25ae284
STNeto1 <gfreitasneto18@gmail.com>
2024-01-26 11:34:51 -0300

    feat: changes to checking account required fields

--- app/Validators/CheckingAccount/UpdateCheckingAccountValidator.ts
+++ app/Validators/CheckingAccount/UpdateCheckingAccountValidator.ts
@@ -1,29 +1,29 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { CustomMessages, schema, rules } from '@ioc:Adonis/Core/Validator';
-import { CheckingAccountType } from 'App/Models/CheckingAccount';
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { CustomMessages, schema, rules } from "@ioc:Adonis/Core/Validator";
+import { CheckingAccountType } from "App/Models/CheckingAccount";
 
 export default class UpdateCheckingAccountValidator {
-  constructor(protected ctx: HttpContextContract) {}
+	constructor(protected ctx: HttpContextContract) {}
 
-  public schema = schema.create({
-    description: schema.string(),
-    accountNumber: schema.string(),
-    bankCode: schema.string(),
-    bankName: schema.string(),
-    agency: schema.string(),
-    type: schema.enum(Object.values(CheckingAccountType)),
-    active: schema.boolean(),
+	public schema = schema.create({
+		description: schema.string(),
+		type: schema.enum(Object.values(CheckingAccountType)),
+		active: schema.boolean(),
 
-    businessUnitId: schema.string.nullableAndOptional([
-      rules.uuid(),
-      rules.exists({ table: 'business_units', column: 'id' }),
-    ]),
-    agencyPhone: schema.string.optional(),
-    managerName: schema.string.optional(),
-    managerPhone: schema.string.optional(),
-    managerEmail: schema.string.optional(),
-    limit: schema.number.optional(),
-  });
+		accountNumber: schema.string.optional(),
+		bankCode: schema.string.optional(),
+		bankName: schema.string.optional(),
+		agency: schema.string.optional(),
+		businessUnitId: schema.string.nullableAndOptional([
+			rules.uuid(),
+			rules.exists({ table: "business_units", column: "id" }),
+		]),
+		agencyPhone: schema.string.optional(),
+		managerName: schema.string.optional(),
+		managerPhone: schema.string.optional(),
+		managerEmail: schema.string.optional(),
+		limit: schema.number.optional(),
+	});
 
-  public messages: CustomMessages = {};
+	public messages: CustomMessages = {};
 }

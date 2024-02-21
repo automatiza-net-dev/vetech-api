eec15de9c6cc92d8526418e5e362dd016eeb9dbc
STNeto1 <gfreitasneto18@gmail.com>
2024-02-21 16:49:18 -0300

    feat: add type to profile access

--- app/Models/ProfileAccess.ts
+++ app/Models/ProfileAccess.ts
@@ -1,24 +1,33 @@
-import { DateTime } from 'luxon';
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm';
+import { DateTime } from "luxon";
+import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
+
+export const ProfileAccessType = [
+	"system",
+	"controller",
+	"user",
+	"both",
+	"all",
+] as const;
+export type TProfileAccessType = (typeof ProfileAccessType)[number];
 
 export default class ProfileAccess extends BaseModel {
-  @column({ isPrimary: true })
-  public id: number;
+	@column({ isPrimary: true })
+	public id: number;
 
-  @column()
-  public description: string;
+	@column()
+	public description: string;
 
-  @column()
-  public active: boolean;
+	@column()
+	public active: boolean;
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime;
+	@column.dateTime({ autoCreate: true })
+	public createdAt: DateTime;
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime;
+	@column.dateTime({ autoCreate: true, autoUpdate: true })
+	public updatedAt: DateTime;
 
-  @column({
-    serializeAs: null,
-  })
-  public system_id: number;
+	@column({
+		serializeAs: null,
+	})
+	public system_id: number;
 }

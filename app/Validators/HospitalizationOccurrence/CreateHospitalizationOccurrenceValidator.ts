9291ddf186ca58d96eaaf3b467edbc22f29e0592
STNeto1 <gfreitasneto18@gmail.com>
2024-03-14 10:18:53 -0300

    feat: add previewed_at to occurrence

--- app/Validators/HospitalizationOccurrence/CreateHospitalizationOccurrenceValidator.ts
+++ app/Validators/HospitalizationOccurrence/CreateHospitalizationOccurrenceValidator.ts
@@ -1,33 +1,34 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator';
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { CustomMessages, rules, schema } from "@ioc:Adonis/Core/Validator";
 
 export default class CreateHospitalizationOccurrenceValidator {
-  constructor(protected ctx: HttpContextContract) {}
+	constructor(protected ctx: HttpContextContract) {}
 
-  public schema = schema.create({
-    hospitalizationId: schema.string({ trim: true }, [
-      rules.uuid(),
-      rules.exists({ table: 'hospitalizations', column: 'id' }),
-    ]),
-    occurrenceId: schema.string({ trim: true }, [
-      rules.uuid(),
-      rules.exists({ table: 'occurrences', column: 'id' }),
-    ]),
-    hospitalizationMedicalPrescriptionId: schema.string.optional(
-      { trim: true },
-      [
-        rules.uuid(),
-        rules.exists({
-          table: 'hospitalization_medical_prescriptions',
-          column: 'id',
-        }),
-      ],
-    ),
-    executedAt: schema.date(),
-    description: schema.string({ trim: true }),
-    resume: schema.string({ trim: true }),
-    attachments: schema.array.optional().members(schema.file()),
-  });
+	public schema = schema.create({
+		hospitalizationId: schema.string({ trim: true }, [
+			rules.uuid(),
+			rules.exists({ table: "hospitalizations", column: "id" }),
+		]),
+		occurrenceId: schema.string({ trim: true }, [
+			rules.uuid(),
+			rules.exists({ table: "occurrences", column: "id" }),
+		]),
+		hospitalizationMedicalPrescriptionId: schema.string.optional(
+			{ trim: true },
+			[
+				rules.uuid(),
+				rules.exists({
+					table: "hospitalization_medical_prescriptions",
+					column: "id",
+				}),
+			],
+		),
+		executedAt: schema.date(),
+		previewedAt: schema.date(),
+		description: schema.string({ trim: true }),
+		resume: schema.string({ trim: true }),
+		attachments: schema.array.optional().members(schema.file()),
+	});
 
-  public messages: CustomMessages = {};
+	public messages: CustomMessages = {};
 }
--- contracts/interfaces/IHospitalizationOccurrenceData.ts
+++ contracts/interfaces/IHospitalizationOccurrenceData.ts
@@ -1,18 +1,19 @@
-import { MultipartFileContract } from '@ioc:Adonis/Core/BodyParser';
-import { DateTime } from 'luxon';
+import { MultipartFileContract } from "@ioc:Adonis/Core/BodyParser";
+import { DateTime } from "luxon";
 
 export default interface IHospitalizationOccurrenceData {
-  hospitalizationId: string;
-  occurrenceId: string;
-  hospitalizationMedicalPrescriptionId?: string;
-  executedAt: DateTime;
-  description: string;
-  resume: string;
-  active: boolean;
-  attachments?: MultipartFileContract[];
+	hospitalizationId: string;
+	occurrenceId: string;
+	hospitalizationMedicalPrescriptionId?: string;
+	executedAt: DateTime;
+	previewedAt: DateTime;
+	description: string;
+	resume: string;
+	active: boolean;
+	attachments?: MultipartFileContract[];
 }
 
 export interface IHospitalizationOccurrenceAttachmentData {
-  occurrenceId: string;
-  attachments: MultipartFileContract[];
+	occurrenceId: string;
+	attachments: MultipartFileContract[];
 }

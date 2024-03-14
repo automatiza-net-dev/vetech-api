75c77e107beeb2a4933fecb4d0cba9673ad70a10
STNeto1 <gfreitasneto18@gmail.com>
2024-03-14 12:19:54 -0300

    fix: file url

--- app/Controllers/Http/AssetsController.ts
+++ app/Controllers/Http/AssetsController.ts
@@ -2,16 +2,20 @@ import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import { inject } from "@adonisjs/fold";
 import CreateLinkValidator from "App/Validators/S3/CreateLinkValidator";
 import Application from "@ioc:Adonis/Core/Application";
+import Drive from "@ioc:Adonis/Core/Drive";
+import Env from "@ioc:Adonis/Core/Env";
 
 @inject()
 export default class AssetsController {
 	public async download({ request, response }: HttpContextContract) {
 		const data = await request.validate(CreateLinkValidator);
 
-		const filePath = Application.tmpPath(data.key);
+		const localFilePath = await Drive.use("local").getUrl(data.key);
+		const fullPath = `${Env.get(
+			"LOCAL_DISK_ROOT",
+			Application.tmpPath(),
+		)}${localFilePath}`.replace("/uploads", "");
 
-		console.log({ filePath });
-
-		return response.download(filePath);
+		return response.download(fullPath);
 	}
 }

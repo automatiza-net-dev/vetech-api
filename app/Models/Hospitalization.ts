44f0b953ff5a65179f6efdf395f88eb17c93c580
STNeto1 <gfreitasneto18@gmail.com>
2024-03-15 10:38:00 -0300

    feat: new hospitalization response

--- app/Models/Hospitalization.ts
+++ app/Models/Hospitalization.ts
@@ -1,168 +1,173 @@
 import {
-  BaseModel,
-  beforeFetch,
-  beforeFind,
-  BelongsTo,
-  belongsTo,
-  column,
-  hasMany,
-  HasMany,
-} from '@ioc:Adonis/Lucid/Orm';
-import Bed from 'App/Models/Bed';
-import HospitalizationClinicParameter from 'App/Models/HospitalizationClinicParameter';
-import HospitalizationMedicalPrescription from 'App/Models/HospitalizationMedicalPrescription';
-import HospitalizationMedicalPrescriptionScheduling from 'App/Models/HospitalizationMedicalPrescriptionScheduling';
-import HospitalizationOccurrence from 'App/Models/HospitalizationOccurrence';
-import Patient from 'App/Models/Patient';
-import User from 'App/Models/User';
-import { softDelete, softDeleteQuery } from 'App/Services/SoftDelete';
-import { DateTime } from 'luxon';
-import { v4 } from 'uuid';
+	BaseModel,
+	beforeFetch,
+	beforeFind,
+	BelongsTo,
+	belongsTo,
+	column,
+	hasMany,
+	HasMany,
+} from "@ioc:Adonis/Lucid/Orm";
+import Bed from "App/Models/Bed";
+import HospitalizationClinicParameter from "App/Models/HospitalizationClinicParameter";
+import HospitalizationMedicalPrescription from "App/Models/HospitalizationMedicalPrescription";
+import HospitalizationMedicalPrescriptionScheduling from "App/Models/HospitalizationMedicalPrescriptionScheduling";
+import HospitalizationOccurrence from "App/Models/HospitalizationOccurrence";
+import Patient from "App/Models/Patient";
+import User from "App/Models/User";
+import { softDelete, softDeleteQuery } from "App/Services/SoftDelete";
+import { DateTime } from "luxon";
+import { v4 } from "uuid";
 
 export enum HospitalizationStatus {
-  'ACTIVE' = 'A',
-  'COMPLETE' = 'C',
+	ACTIVE = "A",
+	COMPLETE = "C",
 }
 
 export enum HospitalizationType {
-  'Internação' = 1,
-  'Observação' = 2,
-  'Uti' = 3,
+	Internação = 1,
+	Observação = 2,
+	Uti = 3,
 }
 
 export const HospitalizationTypeDescription = {
-  [HospitalizationType.Internação]: 'Internação',
-  [HospitalizationType.Observação]: 'Observação',
-  [HospitalizationType.Uti]: 'Uti',
+	[HospitalizationType.Internação]: "Internação",
+	[HospitalizationType.Observação]: "Observação",
+	[HospitalizationType.Uti]: "Uti",
 } as const;
 
 export default class Hospitalization extends BaseModel {
-  @column({ isPrimary: true })
-  public id: string = v4();
+	@column({ isPrimary: true })
+	public id: string = v4();
 
-  @column()
-  public type: HospitalizationType;
+	@column()
+	public type: HospitalizationType;
 
-  @column()
-  public risk: number;
+	@column()
+	public risk: number;
 
-  @column.dateTime({
-    columnName: 'expected_discharge',
-  })
-  public expectedDischarge: DateTime;
+	@column.dateTime({
+		columnName: "expected_discharge",
+	})
+	public expectedDischarge: DateTime;
 
-  @column.dateTime({
-    columnName: 'released_at',
-  })
-  public releasedAt: DateTime;
+	@column.dateTime({
+		columnName: "finished_at",
+	})
+	public finishedAt: DateTime;
 
-  @column.dateTime({
-    columnName: 'death_at',
-  })
-  public deathAt: DateTime;
+	@column.dateTime({
+		columnName: "released_at",
+	})
+	public releasedAt: DateTime;
 
-  @column()
-  public diagnosis: string;
+	@column.dateTime({
+		columnName: "death_at",
+	})
+	public deathAt: DateTime;
 
-  @column()
-  public prognosis: string;
+	@column()
+	public diagnosis: string;
 
-  @column()
-  public complaint: string;
+	@column()
+	public prognosis: string;
 
-  @column()
-  public status: HospitalizationStatus;
+	@column()
+	public complaint: string;
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime;
+	@column()
+	public status: HospitalizationStatus;
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime;
+	@column.dateTime({ autoCreate: true })
+	public createdAt: DateTime;
 
-  @column.dateTime({ serializeAs: null })
-  public deletedAt: DateTime;
+	@column.dateTime({ autoCreate: true, autoUpdate: true })
+	public updatedAt: DateTime;
 
-  @beforeFind()
-  public static softDeletesFind = softDeleteQuery;
+	@column.dateTime({ serializeAs: null })
+	public deletedAt: DateTime;
 
-  @beforeFetch()
-  public static softDeletesFetch = softDeleteQuery;
+	@beforeFind()
+	public static softDeletesFind = softDeleteQuery;
 
-  public async softDelete(column?: string) {
-    await softDelete(this, column);
-  }
+	@beforeFetch()
+	public static softDeletesFetch = softDeleteQuery;
 
-  @column({
-    serializeAs: null,
-  })
-  public economic_group_id: string;
+	public async softDelete(column?: string) {
+		await softDelete(this, column);
+	}
 
-  @column({
-    serializeAs: null,
-  })
-  public business_unit_id: string;
+	@column({
+		serializeAs: null,
+	})
+	public economic_group_id: string;
 
-  @column({
-    serializeAs: null,
-  })
-  public patient_id: string;
+	@column({
+		serializeAs: null,
+	})
+	public business_unit_id: string;
 
-  @belongsTo(() => Patient, {
-    foreignKey: 'patient_id',
-  })
-  public patient: BelongsTo<typeof Patient>;
+	@column({
+		serializeAs: null,
+	})
+	public patient_id: string;
 
-  @column({
-    serializeAs: null,
-  })
-  public tutor_id: string;
+	@belongsTo(() => Patient, {
+		foreignKey: "patient_id",
+	})
+	public patient: BelongsTo<typeof Patient>;
 
-  @belongsTo(() => Patient, {
-    foreignKey: 'tutor_id',
-  })
-  public tutor: BelongsTo<typeof Patient>;
+	@column({
+		serializeAs: null,
+	})
+	public tutor_id: string;
 
-  @column({
-    serializeAs: null,
-  })
-  public technician_id: string;
+	@belongsTo(() => Patient, {
+		foreignKey: "tutor_id",
+	})
+	public tutor: BelongsTo<typeof Patient>;
 
-  @belongsTo(() => User, {
-    foreignKey: 'technician_id',
-  })
-  public technician: BelongsTo<typeof User>;
+	@column({
+		serializeAs: null,
+	})
+	public technician_id: string;
 
-  @column({
-    serializeAs: null,
-  })
-  public bed_id: string;
+	@belongsTo(() => User, {
+		foreignKey: "technician_id",
+	})
+	public technician: BelongsTo<typeof User>;
 
-  @belongsTo(() => Bed, {
-    foreignKey: 'bed_id',
-  })
-  public bed: BelongsTo<typeof Bed>;
+	@column({
+		serializeAs: null,
+	})
+	public bed_id: string;
 
-  @hasMany(() => HospitalizationMedicalPrescription, {
-    foreignKey: 'hospitalization_id',
-  })
-  public medicalPrescriptions: HasMany<
-    typeof HospitalizationMedicalPrescription
-  >;
+	@belongsTo(() => Bed, {
+		foreignKey: "bed_id",
+	})
+	public bed: BelongsTo<typeof Bed>;
 
-  @hasMany(() => HospitalizationOccurrence, {
-    foreignKey: 'hospitalization_id',
-  })
-  public occurrences: HasMany<typeof HospitalizationOccurrence>;
+	@hasMany(() => HospitalizationMedicalPrescription, {
+		foreignKey: "hospitalization_id",
+	})
+	public medicalPrescriptions: HasMany<
+		typeof HospitalizationMedicalPrescription
+	>;
 
-  @hasMany(() => HospitalizationMedicalPrescriptionScheduling, {
-    foreignKey: 'hospitalization_id',
-  })
-  public scheduling: HasMany<
-    typeof HospitalizationMedicalPrescriptionScheduling
-  >;
+	@hasMany(() => HospitalizationOccurrence, {
+		foreignKey: "hospitalization_id",
+	})
+	public occurrences: HasMany<typeof HospitalizationOccurrence>;
 
-  @hasMany(() => HospitalizationClinicParameter, {
-    foreignKey: 'hospitalization_id',
-  })
-  public parameters: HasMany<typeof HospitalizationClinicParameter>;
+	@hasMany(() => HospitalizationMedicalPrescriptionScheduling, {
+		foreignKey: "hospitalization_id",
+	})
+	public scheduling: HasMany<
+		typeof HospitalizationMedicalPrescriptionScheduling
+	>;
+
+	@hasMany(() => HospitalizationClinicParameter, {
+		foreignKey: "hospitalization_id",
+	})
+	public parameters: HasMany<typeof HospitalizationClinicParameter>;
 }

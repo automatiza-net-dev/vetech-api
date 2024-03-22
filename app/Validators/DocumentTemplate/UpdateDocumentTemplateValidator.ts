54e7e8af1e47a55c3a47c86cb53820efaec63689
STNeto1 <gfreitasneto18@gmail.com>
2024-03-22 13:54:28 -0300

    fix: missing validator

--- app/Validators/DocumentTemplate/UpdateDocumentTemplateValidator.ts
+++ app/Validators/DocumentTemplate/UpdateDocumentTemplateValidator.ts
@@ -1,16 +1,17 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { CustomMessages, schema } from '@ioc:Adonis/Core/Validator';
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { CustomMessages, schema } from "@ioc:Adonis/Core/Validator";
 
 export default class UpdateDocumentTemplateValidator {
-  constructor(protected ctx: HttpContextContract) {}
+	constructor(protected ctx: HttpContextContract) {}
 
-  public schema = schema.create({
-    title: schema.string({}, []),
-    description: schema.string({}, []),
-    header: schema.string.optional({}, []),
-    template: schema.string({}, []),
-    active: schema.boolean([]),
-  });
+	public schema = schema.create({
+		title: schema.string({}, []),
+		description: schema.string({}, []),
+		header: schema.string.optional({}, []),
+		template: schema.string({}, []),
+		file: schema.file.optional({ extnames: ["doc", "docx"] }),
+		active: schema.boolean([]),
+	});
 
-  public messages: CustomMessages = {};
+	public messages: CustomMessages = {};
 }

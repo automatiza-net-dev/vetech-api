08c44233eda815f7c1c94abf9b2e6ee621683df8
STNeto1 <gfreitasneto18@gmail.com>
2024-04-08 11:50:22 -0300

    fix: add missing field to model

--- app/Models/ScheduleServiceType.ts
+++ app/Models/ScheduleServiceType.ts
@@ -1,95 +1,98 @@
 import {
-  BaseModel,
-  beforeFetch,
-  beforeFind,
-  BelongsTo,
-  belongsTo,
-  column,
-  HasMany,
-  hasMany,
-} from '@ioc:Adonis/Lucid/Orm';
-import EconomicGroup from 'App/Models/EconomicGroup';
-import Product from 'App/Models/Product';
-import Schedule from 'App/Models/Schedule';
-import ScheduleServiceGroup from 'App/Models/ScheduleServiceGroup';
-import { softDelete, softDeleteQuery } from 'App/Services/SoftDelete';
-import { DateTime } from 'luxon';
-import { v4 } from 'uuid';
+	BaseModel,
+	beforeFetch,
+	beforeFind,
+	BelongsTo,
+	belongsTo,
+	column,
+	HasMany,
+	hasMany,
+} from "@ioc:Adonis/Lucid/Orm";
+import EconomicGroup from "App/Models/EconomicGroup";
+import Product from "App/Models/Product";
+import Schedule from "App/Models/Schedule";
+import ScheduleServiceGroup from "App/Models/ScheduleServiceGroup";
+import { softDelete, softDeleteQuery } from "App/Services/SoftDelete";
+import { DateTime } from "luxon";
+import { v4 } from "uuid";
 
 export default class ScheduleServiceType extends BaseModel {
-  @column({ isPrimary: true })
-  public id: string = v4();
+	@column({ isPrimary: true })
+	public id: string = v4();
 
-  @column({
-    columnName: 'reserved_minutes',
-  })
-  public reservedMinutes: number;
+	@column({
+		columnName: "reserved_minutes",
+	})
+	public reservedMinutes: number;
 
-  @column()
-  public description: string;
+	@column()
+	public description: string;
 
-  @column()
-  public resume: string;
+	@column()
+	public type: string;
 
-  @column({
-    columnName: 'allow_return',
-  })
-  public allowReturn: boolean;
+	@column()
+	public resume: string;
 
-  @column()
-  public active: boolean;
+	@column({
+		columnName: "allow_return",
+	})
+	public allowReturn: boolean;
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime;
+	@column()
+	public active: boolean;
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime;
+	@column.dateTime({ autoCreate: true })
+	public createdAt: DateTime;
 
-  @column.dateTime({ serializeAs: null })
-  public deletedAt: DateTime;
+	@column.dateTime({ autoCreate: true, autoUpdate: true })
+	public updatedAt: DateTime;
 
-  @beforeFind()
-  public static softDeletesFind = softDeleteQuery;
+	@column.dateTime({ serializeAs: null })
+	public deletedAt: DateTime;
 
-  @beforeFetch()
-  public static softDeletesFetch = softDeleteQuery;
+	@beforeFind()
+	public static softDeletesFind = softDeleteQuery;
 
-  public async softDelete(column?: string) {
-    await softDelete(this, column);
-  }
+	@beforeFetch()
+	public static softDeletesFetch = softDeleteQuery;
 
-  @column({
-    serializeAs: null,
-  })
-  public system_id: number;
+	public async softDelete(column?: string) {
+		await softDelete(this, column);
+	}
 
-  @column()
-  public economic_group_id?: string;
+	@column({
+		serializeAs: null,
+	})
+	public system_id: number;
 
-  @belongsTo(() => EconomicGroup, {})
-  public group: BelongsTo<typeof EconomicGroup>;
+	@column()
+	public economic_group_id?: string;
 
-  @column()
-  public schedule_service_group_id: string;
+	@belongsTo(() => EconomicGroup, {})
+	public group: BelongsTo<typeof EconomicGroup>;
 
-  @belongsTo(() => ScheduleServiceGroup, {
-    localKey: 'id',
-    foreignKey: 'schedule_service_group_id',
-  })
-  public serviceGroup: BelongsTo<typeof ScheduleServiceGroup>;
+	@column()
+	public schedule_service_group_id: string;
 
-  @hasMany(() => Schedule, {
-    localKey: 'id',
-    foreignKey: 'schedule_service_type_id',
-  })
-  public schedules: HasMany<typeof Schedule>;
+	@belongsTo(() => ScheduleServiceGroup, {
+		localKey: "id",
+		foreignKey: "schedule_service_group_id",
+	})
+	public serviceGroup: BelongsTo<typeof ScheduleServiceGroup>;
 
-  @column()
-  public product_id: string;
+	@hasMany(() => Schedule, {
+		localKey: "id",
+		foreignKey: "schedule_service_type_id",
+	})
+	public schedules: HasMany<typeof Schedule>;
 
-  @belongsTo(() => Product, {
-    localKey: 'id',
-    foreignKey: 'product_id',
-  })
-  public product: BelongsTo<typeof Product>;
+	@column()
+	public product_id: string;
+
+	@belongsTo(() => Product, {
+		localKey: "id",
+		foreignKey: "product_id",
+	})
+	public product: BelongsTo<typeof Product>;
 }

c5adb8c38445fea67e3bcb53d7bd1566b6bb339f
STNeto1 <gfreitasneto18@gmail.com>
2024-05-15 09:17:23 -0300

    fix: indicator comparison

--- app/Services/IndicatorService.ts
+++ app/Services/IndicatorService.ts
@@ -3475,7 +3475,7 @@ export default class IndicatorService {
 			.groupByRaw(`business_units.id`)
 			.whereNull("bills.deleted_at")
 			.whereRaw(
-				`to_char(bills.bill_date, 'YYYY-MM') <> to_char(patients.first_sale, 'YYYY-MM')`,
+				`to_char(bills.bill_date, 'YYYY-MM') = to_char(patients.first_sale, 'YYYY-MM')`,
 			)
 			.whereRaw(`business_units.environment = 'P'`);
 
@@ -3568,6 +3568,9 @@ export default class IndicatorService {
 					grupo: elem.grupo ?? "Não informado",
 					total: elem.total,
 					porcentagem: (rowsSum / parseFloat(elem.total)) * 100,
+					origens: result
+						.filter((r) => r.categoria === curr)
+						.map((ori) => ({ descricao: ori.description })),
 				})),
 			});
 

8fb19b2b0f8800e0a6e21017387737e848c91afc
STNeto1 <gfreitasneto18@gmail.com>
2024-05-17 09:24:34 -0300

    fix: query

--- app/Services/ScheduleService.ts
+++ app/Services/ScheduleService.ts
@@ -1684,7 +1684,9 @@ export default class ScheduleService {
 			const toStatus = await ScheduleStatus.query()
 				.useTransaction(trx)
 				.whereRaw("(system_id = ? or system_id is null)", [authCtx.system.id])
-				.whereRaw("economic_group_id = ?", [authCtx.group.id])
+				.whereRaw("(economic_group_id = ? or economic_group_id is null)", [
+					authCtx.group.id,
+				])
 				.where("type", data.scheduleStatusType)
 				.first();
 			if (!toStatus) {

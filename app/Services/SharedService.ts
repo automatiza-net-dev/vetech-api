1f60c005043979da63b146cf64121f93477ad951
STNeto1 <gfreitasneto18@gmail.com>
2024-06-05 14:48:36 -0300

    fix: sql

--- app/Services/SharedService.ts
+++ app/Services/SharedService.ts
@@ -151,9 +151,10 @@ export default class SharedService {
 		permissionControlID: string,
 	): Promise<boolean> {
 		const rows = await Database.from("user_unit_roles")
+			.debug(true)
 			.select(Database.raw("1"))
 			.joinRaw("join roles on roles.id = user_unit_roles.role_id")
-			.joinRaw("join role_permissions on roles.id = roles.id")
+			.joinRaw("join role_permissions on role_permissions.id = roles.id")
 			.joinRaw(
 				"join permissions on role_permissions.permission_id = permissions.id and permissions.control_id = ?",
 				[permissionControlID],
@@ -164,7 +165,8 @@ export default class SharedService {
 			.where("roles.active", true)
 			.where("role_permissions.active", true)
 			.where("role_permissions.status", true)
-			.whereIn("roles.type", ["controller", "both", "all", "user"]);
+			.whereIn("roles.type", ["controller", "both", "all", "user"])
+			.whereIn("permissions.type", ["controller", "both", "all", "user"]);
 
 		return rows.length > 0;
 	}

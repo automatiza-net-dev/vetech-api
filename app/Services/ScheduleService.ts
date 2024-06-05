5ccd6c8f8449b4ccf7a1627ed67598c713d579af
STNeto1 <gfreitasneto18@gmail.com>
2024-06-05 11:50:17 -0300

    fix: tag

--- app/Services/ScheduleService.ts
+++ app/Services/ScheduleService.ts
@@ -530,7 +530,7 @@ export default class ScheduleService {
 				query.select(["id", "description", "color", "type"]);
 			})
 			.preload("patient", (query) => {
-				query.select(["id", "name", "gender"]);
+				query.select(["id", "name", "gender", "tag"]);
 			})
 			.preload("holder", (query) => {
 				query.select(["id", "name"]);

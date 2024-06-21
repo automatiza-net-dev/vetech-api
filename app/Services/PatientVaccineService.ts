b01938b8f10e3e939e401f1e90c0ac3847851f55
STNeto1 <gfreitasneto18@gmail.com>
2024-06-21 10:44:10 -0300

    feat: vermifuge on timeline

--- app/Services/PatientVaccineService.ts
+++ app/Services/PatientVaccineService.ts
@@ -1,203 +1,213 @@
-import { inject } from '@adonisjs/fold';
-import Database from '@ioc:Adonis/Lucid/Database';
-import ResourceNotFoundException from 'App/Exceptions/ResourceNotFoundException';
-import AnimalTimeline from 'App/Models/mongoose/AnimalTimeline';
-import TimelineType from 'App/Models/TimelineType';
-import User from 'App/Models/User';
-import VaccineCalendar from 'App/Models/VaccineCalendar';
-import VaccineProtocol from 'App/Models/VaccineProtocol';
-import { AuthContext } from 'App/Services/SharedService';
-import IPatientVaccineData from 'Contracts/interfaces/IPatientVaccineData';
-import { DateTime } from 'luxon';
+import { inject } from "@adonisjs/fold";
+import Database from "@ioc:Adonis/Lucid/Database";
+import ResourceNotFoundException from "App/Exceptions/ResourceNotFoundException";
+import AnimalTimeline from "App/Models/mongoose/AnimalTimeline";
+import TimelineType from "App/Models/TimelineType";
+import User from "App/Models/User";
+import { VaccineType } from "App/Models/Vaccine";
+import VaccineCalendar from "App/Models/VaccineCalendar";
+import VaccineProtocol from "App/Models/VaccineProtocol";
+import { AuthContext } from "App/Services/SharedService";
+import IPatientVaccineData from "Contracts/interfaces/IPatientVaccineData";
+import { DateTime } from "luxon";
 
-import PatientVaccine from '../Models/PatientVaccine';
+import PatientVaccine from "../Models/PatientVaccine";
 
 type ISearch = {
-  vaccine?: string;
-  protocol?: string;
-  patient?: string;
+	vaccine?: string;
+	protocol?: string;
+	patient?: string;
 };
 
 @inject()
 export default class PatientVaccineService {
-  public async index(unitId: string, data: ISearch) {
-    const qb = PatientVaccine.query();
+	public async index(unitId: string, data: ISearch) {
+		const qb = PatientVaccine.query();
 
-    qb.where('business_unit_id', unitId);
+		qb.where("business_unit_id", unitId);
 
-    if (data.vaccine) {
-      qb.where('vaccine_id', data.vaccine);
-    }
+		if (data.vaccine) {
+			qb.where("vaccine_id", data.vaccine);
+		}
 
-    if (data.protocol) {
-      qb.where('vaccine_protocol_id', data.protocol);
-    }
+		if (data.protocol) {
+			qb.where("vaccine_protocol_id", data.protocol);
+		}
 
-    if (data.patient) {
-      qb.where('patient_id', data.patient);
-    }
+		if (data.patient) {
+			qb.where("patient_id", data.patient);
+		}
 
-    await Promise.all([
-      qb.preload('vaccine'),
-      qb.preload('protocol'),
-      qb.preload('patient'),
-      qb.preload('user', query => {
-        query.select('id', 'name', 'email');
-      }),
-      qb.preload('schedule'),
-      qb.preload('calendars', query => {
-        query.orderBy('dose');
-      }),
-    ]);
+		await Promise.all([
+			qb.preload("vaccine"),
+			qb.preload("protocol"),
+			qb.preload("patient"),
+			qb.preload("user", (query) => {
+				query.select("id", "name", "email");
+			}),
+			qb.preload("schedule"),
+			qb.preload("calendars", (query) => {
+				query.orderBy("dose");
+			}),
+		]);
 
-    return qb;
-  }
+		return qb;
+	}
 
-  public async store(authCtx: AuthContext, data: IPatientVaccineData) {
-    return Database.transaction(async trx => {
-      const protocol = await VaccineProtocol.query()
-        .where('id', data.vaccineProtocolId)
-        .preload('vaccine')
-        .firstOrFail();
+	public async store(authCtx: AuthContext, data: IPatientVaccineData) {
+		return Database.transaction(async (trx) => {
+			const protocol = await VaccineProtocol.query()
+				.where("id", data.vaccineProtocolId)
+				.preload("vaccine")
+				.firstOrFail();
 
-      const entity = await PatientVaccine.create(
-        {
-          business_unit_id: authCtx.unit.id,
-          user_id: data.userId ?? authCtx.user.id,
-          patient_id: data.patientId,
-          vaccine_id: data.vaccineId,
-          vaccine_protocol_id: data.vaccineProtocolId,
-          schedule_id: data.scheduleId,
-        },
-        {
-          client: trx,
-        },
-      );
+			const entity = await PatientVaccine.create(
+				{
+					business_unit_id: authCtx.unit.id,
+					user_id: data.userId ?? authCtx.user.id,
+					patient_id: data.patientId,
+					vaccine_id: data.vaccineId,
+					vaccine_protocol_id: data.vaccineProtocolId,
+					schedule_id: data.scheduleId,
+				},
+				{
+					client: trx,
+				},
+			);
 
-      const calendars: Array<Partial<VaccineCalendar>> = Array.from(
-        { length: protocol.doses },
-        (_, index) => {
-          const schedulingDate = data.applications?.find(
-            application => application.dose === index + 1,
-          )?.date;
+			const calendars: Array<Partial<VaccineCalendar>> = Array.from(
+				{ length: protocol.doses },
+				(_, index) => {
+					const schedulingDate = data.applications?.find(
+						(application) => application.dose === index + 1,
+					)?.date;
 
-          return {
-            schedulingDate:
-              schedulingDate ??
-              DateTime.now().plus({
-                days: index * protocol.interval,
-              }),
-            dose: index + 1,
-            schedule_id: data.scheduleId,
-            user_id: data.userId ?? authCtx.user.id,
-          };
-        },
-      );
+					return {
+						schedulingDate:
+							schedulingDate ??
+							DateTime.now().plus({
+								days: index * protocol.interval,
+							}),
+						dose: index + 1,
+						schedule_id: data.scheduleId,
+						user_id: data.userId ?? authCtx.user.id,
+					};
+				},
+			);
 
-      const dbCalendars = await entity
-        .related('calendars')
-        .createMany(calendars, trx);
+			const dbCalendars = await entity
+				.related("calendars")
+				.createMany(calendars, trx);
 
-      const vaccineTimeline = await TimelineType.firstOrCreate(
-        {
-          description: 'Vacinas',
-          system_id: authCtx.system.id,
-        },
-        {
-          description: 'Vacinas',
-          color: '#000',
-          requiresObservation: false,
-          system_id: authCtx.system.id,
-        },
-        {
-          client: trx,
-        },
-      );
+			const vaccineTimeline = await TimelineType.firstOrCreate(
+				{
+					description:
+						protocol.vaccine.type === VaccineType.VACCINE
+							? "Vacinas"
+							: "Vermifugos",
+					system_id: authCtx.system.id,
+				},
+				{
+					description:
+						protocol.vaccine.type === VaccineType.VACCINE
+							? "Vacinas"
+							: "Vermifugos",
+					color: "#000",
+					requiresObservation: false,
+					system_id: authCtx.system.id,
+				},
+				{
+					client: trx,
+				},
+			);
 
-      await AnimalTimeline.create({
-        timeline_id: vaccineTimeline.id,
-        timeline_type: {
-          description: vaccineTimeline.description,
-          color: vaccineTimeline.color,
-          requires_observation: vaccineTimeline.requiresObservation,
-        },
-        timeline_info: {
-          tag: entity.patient_id,
-          origin: 'new_vaccine',
-          patient_vaccine: {
-            id: entity.id,
-          },
-          technician: {
-            id: authCtx.user.id,
-            name: authCtx.user.name,
-          },
-          vaccine: {
-            id: protocol.vaccine.id,
-            name: protocol.vaccine.name,
-            description: protocol.vaccine.description,
-            origin: 'new',
-          },
-          protocol: {
-            id: protocol.id,
-          },
-          issuedAt: new Date(),
-          startsAt: dbCalendars.at(0)?.schedulingDate,
-        },
-      });
+			await AnimalTimeline.create({
+				timeline_id: vaccineTimeline.id,
+				timeline_type: {
+					description: vaccineTimeline.description,
+					color: vaccineTimeline.color,
+					requires_observation: vaccineTimeline.requiresObservation,
+				},
+				timeline_info: {
+					tag: entity.patient_id,
+					origin:
+						protocol.vaccine.type === VaccineType.VACCINE
+							? "new_vaccine"
+							: "new_vermifuge",
+					patient_vaccine: {
+						id: entity.id,
+					},
+					technician: {
+						id: authCtx.user.id,
+						name: authCtx.user.name,
+					},
+					vaccine: {
+						id: protocol.vaccine.id,
+						name: protocol.vaccine.name,
+						description: protocol.vaccine.description,
+						origin: "new",
+					},
+					protocol: {
+						id: protocol.id,
+					},
+					issuedAt: new Date(),
+					startsAt: dbCalendars.at(0)?.schedulingDate,
+				},
+			});
 
-      return entity;
-    });
-  }
+			return entity;
+		});
+	}
 
-  public async show(unitId: string, id: string) {
-    const qb = PatientVaccine.query()
-      .where('id', id)
-      .where('business_unit_id', unitId)
-      .preload('vaccine', query => {
-        query.preload('subgroup');
-      })
-      .preload('protocol')
-      .preload('patient')
-      .preload('user', query => {
-        query.select('id', 'name', 'email');
-      })
-      .preload('schedule')
-      .preload('calendars');
+	public async show(unitId: string, id: string) {
+		const qb = PatientVaccine.query()
+			.where("id", id)
+			.where("business_unit_id", unitId)
+			.preload("vaccine", (query) => {
+				query.preload("subgroup");
+			})
+			.preload("protocol")
+			.preload("patient")
+			.preload("user", (query) => {
+				query.select("id", "name", "email");
+			})
+			.preload("schedule")
+			.preload("calendars");
 
-    const entity = await qb.first();
+		const entity = await qb.first();
 
-    if (!entity) {
-      throw new ResourceNotFoundException('Recurso não encontrado');
-    }
+		if (!entity) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+		}
 
-    return entity;
-  }
+		return entity;
+	}
 
-  public async update(
-    unitId: string,
-    id: string,
-    user: User,
-    data: Omit<IPatientVaccineData, 'applications'>,
-  ) {
-    const entity = await this.show(unitId, id);
+	public async update(
+		unitId: string,
+		id: string,
+		user: User,
+		data: Omit<IPatientVaccineData, "applications">,
+	) {
+		const entity = await this.show(unitId, id);
 
-    entity.merge({
-      business_unit_id: unitId,
-      user_id: data.userId ?? user.id,
-      patient_id: data.patientId,
-      vaccine_id: data.vaccineId,
-      vaccine_protocol_id: data.vaccineProtocolId,
-      schedule_id: data.scheduleId,
-    });
+		entity.merge({
+			business_unit_id: unitId,
+			user_id: data.userId ?? user.id,
+			patient_id: data.patientId,
+			vaccine_id: data.vaccineId,
+			vaccine_protocol_id: data.vaccineProtocolId,
+			schedule_id: data.scheduleId,
+		});
 
-    await entity.save();
+		await entity.save();
 
-    return entity;
-  }
+		return entity;
+	}
 
-  public async destroy(unitId: string, id: string) {
-    const entity = await this.show(unitId, id);
+	public async destroy(unitId: string, id: string) {
+		const entity = await this.show(unitId, id);
 
-    await entity.softDelete();
-  }
+		await entity.softDelete();
+	}
 }
--- app/Services/VaccineCalendarService.ts
+++ app/Services/VaccineCalendarService.ts
@@ -1,171 +1,181 @@
-import { inject } from '@adonisjs/fold';
-import Database from '@ioc:Adonis/Lucid/Database';
-import BadRequestException from 'App/Exceptions/BadRequestException';
-import ResourceNotFoundException from 'App/Exceptions/ResourceNotFoundException';
-import AnimalTimeline from 'App/Models/mongoose/AnimalTimeline';
-import TimelineType from 'App/Models/TimelineType';
-import VaccineCalendar from 'App/Models/VaccineCalendar';
-import { AuthContext } from 'App/Services/SharedService';
-import { IVaccineCalendarData } from 'Contracts/interfaces/IVaccineCalendarData';
-import { DateTime } from 'luxon';
+import { inject } from "@adonisjs/fold";
+import Database from "@ioc:Adonis/Lucid/Database";
+import BadRequestException from "App/Exceptions/BadRequestException";
+import ResourceNotFoundException from "App/Exceptions/ResourceNotFoundException";
+import AnimalTimeline from "App/Models/mongoose/AnimalTimeline";
+import TimelineType from "App/Models/TimelineType";
+import { VaccineType } from "App/Models/Vaccine";
+import VaccineCalendar from "App/Models/VaccineCalendar";
+import { AuthContext } from "App/Services/SharedService";
+import { IVaccineCalendarData } from "Contracts/interfaces/IVaccineCalendarData";
+import { DateTime } from "luxon";
 
 type ISearch = {
-  patient: string;
-  vaccineProtocol: string;
-  vaccine?: string;
-  schedule?: string;
-  scheduleDate?: string;
-  applicationDate?: string;
+	patient: string;
+	vaccineProtocol: string;
+	vaccine?: string;
+	schedule?: string;
+	scheduleDate?: string;
+	applicationDate?: string;
 };
 
 @inject()
 export default class VaccineCalendarService {
-  async index(data: ISearch) {
-    const qb = VaccineCalendar.query();
+	async index(data: ISearch) {
+		const qb = VaccineCalendar.query();
 
-    if (!data.vaccineProtocol || !data.patient) {
-      throw new BadRequestException('Parâmetros inválidos');
-    }
+		if (!data.vaccineProtocol || !data.patient) {
+			throw new BadRequestException("Parâmetros inválidos");
+		}
 
-    qb.preload('patientVaccine', query => {
-      query.preload('vaccine');
-      query.preload('protocol');
-    });
+		qb.preload("patientVaccine", (query) => {
+			query.preload("vaccine");
+			query.preload("protocol");
+		});
 
-    qb.whereHas('patientVaccine', query => {
-      query.where('patient_id', data.patient);
-      query.where('vaccine_protocol_id', data.vaccineProtocol);
+		qb.whereHas("patientVaccine", (query) => {
+			query.where("patient_id", data.patient);
+			query.where("vaccine_protocol_id", data.vaccineProtocol);
 
-      if (data.vaccine) {
-        query.where('vaccine_id', data.vaccine);
-      }
-    });
+			if (data.vaccine) {
+				query.where("vaccine_id", data.vaccine);
+			}
+		});
 
-    if (data.schedule) {
-      qb.where('schedule_id', data.schedule);
-    }
+		if (data.schedule) {
+			qb.where("schedule_id", data.schedule);
+		}
 
-    if (data.scheduleDate) {
-      const date = DateTime.fromISO(data.scheduleDate);
+		if (data.scheduleDate) {
+			const date = DateTime.fromISO(data.scheduleDate);
 
-      qb.whereBetween('scheduling_date', [
-        date.startOf('day').toISODate()!,
-        date.endOf('day').toISODate()!,
-      ]);
-    }
+			qb.whereBetween("scheduling_date", [
+				date.startOf("day").toISODate()!,
+				date.endOf("day").toISODate()!,
+			]);
+		}
 
-    if (data.applicationDate) {
-      const date = DateTime.fromISO(data.applicationDate);
+		if (data.applicationDate) {
+			const date = DateTime.fromISO(data.applicationDate);
 
-      qb.whereBetween('application_date', [
-        date.startOf('day').toISODate()!,
-        date.endOf('day').toISODate()!,
-      ]);
-    }
+			qb.whereBetween("application_date", [
+				date.startOf("day").toISODate()!,
+				date.endOf("day").toISODate()!,
+			]);
+		}
 
-    return qb;
-  }
+		return qb;
+	}
 
-  async update(authCtx: AuthContext, id: string, data: IVaccineCalendarData) {
-    return Database.transaction(async trx => {
-      const calendar = await VaccineCalendar.query()
-        .useTransaction(trx)
-        .where('id', id)
-        .preload('patientVaccine', query => {
-          query.preload('vaccine');
-          query.preload('protocol');
-          query.preload('patient');
-        })
-        .first();
+	async update(authCtx: AuthContext, id: string, data: IVaccineCalendarData) {
+		return Database.transaction(async (trx) => {
+			const calendar = await VaccineCalendar.query()
+				.useTransaction(trx)
+				.where("id", id)
+				.preload("patientVaccine", (query) => {
+					query.preload("vaccine");
+					query.preload("protocol");
+					query.preload("patient");
+				})
+				.first();
 
-      if (!calendar) {
-        throw new ResourceNotFoundException(
-          'Calendário de vacinação não encontrado',
-        );
-      }
+			if (!calendar) {
+				throw new ResourceNotFoundException(
+					"Calendário de vacinação não encontrado",
+				);
+			}
 
-      await calendar
-        .merge({
-          schedulingDate: data.schedulingDate ?? calendar.schedulingDate,
-          applicationDate: data.applicationDate,
-          dose: data.dose,
-          laboratory: data.laboratory,
-          batch: data.batch,
-          product_id: data.productId,
-        })
-        .useTransaction(trx)
-        .save();
+			await calendar
+				.merge({
+					schedulingDate: data.schedulingDate ?? calendar.schedulingDate,
+					applicationDate: data.applicationDate,
+					dose: data.dose,
+					laboratory: data.laboratory,
+					batch: data.batch,
+					product_id: data.productId,
+				})
+				.useTransaction(trx)
+				.save();
 
-      const timeline = await TimelineType.firstOrCreate(
-        {
-          description: 'Vacinas',
-          system_id: authCtx.system.id,
-        },
-        {
-          description: 'Vacinas',
-          color: '#000',
-          requiresObservation: false,
-          system_id: authCtx.system.id,
-        },
-        {
-          client: trx,
-        },
-      );
+			const timeline = await TimelineType.firstOrCreate(
+				{
+					description:
+						calendar.patientVaccine.vaccine.type === VaccineType.VACCINE
+							? "Vacinas"
+							: "Vermifugos",
+					system_id: authCtx.system.id,
+				},
+				{
+					description:
+						calendar.patientVaccine.vaccine.type === VaccineType.VACCINE
+							? "Vacinas"
+							: "Vermifugos",
+					color: "#000",
+					requiresObservation: false,
+					system_id: authCtx.system.id,
+				},
+				{
+					client: trx,
+				},
+			);
 
-      await AnimalTimeline.create({
-        timeline_id: timeline.id,
-        timeline_type: {
-          description: timeline.description,
-          color: timeline.color,
-          requires_observation: timeline.requiresObservation,
-        },
-        timeline_info: {
-          tag: calendar.patientVaccine.patient_id,
-          origin: 'vaccine_application',
-          patient_vaccine: {
-            id: calendar.patientVaccine.id,
-          },
-          technician: {
-            id: authCtx.user.id,
-            name: authCtx.user.name,
-          },
-          vaccine: {
-            id: calendar.patientVaccine.vaccine.id,
-            name: calendar.patientVaccine.vaccine.name,
-            description: calendar.patientVaccine.vaccine.description,
-            origin: 'application',
-          },
-          protocol: {
-            id: calendar.patientVaccine.protocol.id,
-          },
-          schedulingDate: calendar.schedulingDate,
-          applicationDate: data.applicationDate,
-          dose: data.dose,
-          laboratory: data.laboratory,
-          batch: data.batch,
-        },
-      });
+			await AnimalTimeline.create({
+				timeline_id: timeline.id,
+				timeline_type: {
+					description: timeline.description,
+					color: timeline.color,
+					requires_observation: timeline.requiresObservation,
+				},
+				timeline_info: {
+					tag: calendar.patientVaccine.patient_id,
+					origin:
+						calendar.patientVaccine.vaccine.type === VaccineType.VACCINE
+							? "vaccine_application"
+							: "vermifuge_application",
+					patient_vaccine: {
+						id: calendar.patientVaccine.id,
+					},
+					technician: {
+						id: authCtx.user.id,
+						name: authCtx.user.name,
+					},
+					vaccine: {
+						id: calendar.patientVaccine.vaccine.id,
+						name: calendar.patientVaccine.vaccine.name,
+						description: calendar.patientVaccine.vaccine.description,
+						origin: "application",
+					},
+					protocol: {
+						id: calendar.patientVaccine.protocol.id,
+					},
+					schedulingDate: calendar.schedulingDate,
+					applicationDate: data.applicationDate,
+					dose: data.dose,
+					laboratory: data.laboratory,
+					batch: data.batch,
+				},
+			});
 
-      return calendar;
-    });
-  }
+			return calendar;
+		});
+	}
 
-  async destroy(id: string) {
-    const calendar = await VaccineCalendar.find(id);
+	async destroy(id: string) {
+		const calendar = await VaccineCalendar.find(id);
 
-    if (!calendar) {
-      throw new ResourceNotFoundException(
-        'Calendário de vacinação não encontrado',
-      );
-    }
+		if (!calendar) {
+			throw new ResourceNotFoundException(
+				"Calendário de vacinação não encontrado",
+			);
+		}
 
-    calendar.merge({
-      applicationDate: null,
-      laboratory: null,
-      batch: null,
-      product_id: null,
-    });
+		calendar.merge({
+			applicationDate: null,
+			laboratory: null,
+			batch: null,
+			product_id: null,
+		});
 
-    return calendar.save();
-  }
+		return calendar.save();
+	}
 }

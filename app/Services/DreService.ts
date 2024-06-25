7c6339c333fa679c31e43d97bebaef35edb9f37b
STNeto1 <gfreitasneto18@gmail.com>
2024-06-25 15:53:48 -0300

    feat: correct data source

--- app/Services/DreService.ts
+++ app/Services/DreService.ts
@@ -32,80 +32,42 @@ export default class DreService {
 	}
 
 	public async generateDreSpreadsheet(authCtx: AuthContext) {
-		// return Database.from("finances")
-		// 	.select(
-		// 		Database.raw(`substring(competence_date, 1, 2)                                                      mes,
-		//      substring(competence_date, 4, 4)                                                      ano,
-		//      ('01' || '/' || competence_date) as                                                   data,
-		//      case when pc.parent_id is null then pc.description else pcPai.description end         plano_contas_grupo,
-		//      finances.historic                                                                            historico,
-		//      patients."name"                                                                              Pessoa,
-		//      ''                                                                                    col_g,
-		//      ''                                                                                    col_h,
-		//      ''                                                                                    col_i,
-		//      ''                                                                                    col_j,
-		//      ''                                                                                    col_k,
-		//      case when finances."type" = 'DEBITO' then TO_CHAR(finances.total_value, '9999990D99') else '0' end  valor_pago,
-		//      case when finances."type" = 'CREDITO' then TO_CHAR(finances.total_value, '9999990D99') else '0' end valor_recebido,
-		//      case
-		//          when finances."type" = 'DEBITO' then TO_CHAR(finances.total_value * (-1), '9999990D99')
-		//          else TO_CHAR(finances.total_value, '9999990D99') end                                     total,
-		//      pc.description                                                                        plano_contas`),
-		// 	)
-		// 	.joinRaw(`left join (account_plans pc left join account_plan_groups gpc on pc.account_plan_group_id = gpc.id
-		//   left join account_plans pcPai on pc.parent_id = pcPai.id) on finances.account_plan_id = pc."id"`)
-		// 	.joinRaw('join patients on finances.client_id = patients."id"')
-		// 	.joinRaw(
-		// 		"join economic_groups on finances.economic_group_id = economic_groups.id",
-		// 	)
-		// 	.joinRaw("join systems on economic_groups.system_id = systems.id")
-		// 	.joinRaw(
-		// 		"join business_units on finances.business_unit_id = business_units.id",
-		// 	)
-		// 	.whereIn("finances.business_unit_id", [authCtx.unit.id])
-		// 	.whereNull("finances.deleted_at")
-		// 	.where("finances.competence_date", `0${4}/2024`)
-		// 	.orderByRaw(
-		// 		'finances."type", finances.issue_date, finances."document", finances.installment',
-		// 	);
-
-		const data: DreRow[] = [
-			{
-				mes: "04",
-				ano: "2024",
-				data: "01/04/2024",
-				plano_contas_grupo: "Receitas Financeiras",
-				historico: null,
-				pessoa: "clark kent",
-				col_g: "",
-				col_h: "",
-				col_i: "",
-				col_j: "",
-				col_k: "",
-				valor_pago: "0",
-				valor_recebido: "     250,00",
-				total: "     250,00",
-				plano_contas: "Receitas Financeiras",
-			},
-			{
-				mes: "04",
-				ano: "2024",
-				data: "01/04/2024",
-				plano_contas_grupo: "Receitas Produtos",
-				historico: null,
-				pessoa: "alberto luciano",
-				col_g: "",
-				col_h: "",
-				col_i: "",
-				col_j: "",
-				col_k: "",
-				valor_pago: "0",
-				valor_recebido: "     150,00",
-				total: "     150,00",
-				plano_contas: "Receitas Produtos",
-			},
-		];
-
+		const data = await Database.from("finances")
+			.select(
+				Database.raw(`substring(competence_date, 1, 2)                                                      mes,
+		     substring(competence_date, 4, 4)                                                      ano,
+		     ('01' || '/' || competence_date) as                                                   data,
+		     case when pc.parent_id is null then pc.description else pcPai.description end         plano_contas_grupo,
+		     finances.historic                                                                            historico,
+		     patients."name"                                                                              Pessoa,
+		     ''                                                                                    col_g,
+		     ''                                                                                    col_h,
+		     ''                                                                                    col_i,
+		     ''                                                                                    col_j,
+		     ''                                                                                    col_k,
+		     case when finances."type" = 'DEBITO' then TO_CHAR(finances.total_value, '9999990D99') else '0' end  valor_pago,
+		     case when finances."type" = 'CREDITO' then TO_CHAR(finances.total_value, '9999990D99') else '0' end valor_recebido,
+		     case
+		         when finances."type" = 'DEBITO' then TO_CHAR(finances.total_value * (-1), '9999990D99')
+		         else TO_CHAR(finances.total_value, '9999990D99') end                                     total,
+		     pc.description                                                                        plano_contas`),
+			)
+			.joinRaw(`left join (account_plans pc left join account_plan_groups gpc on pc.account_plan_group_id = gpc.id
+		  left join account_plans pcPai on pc.parent_id = pcPai.id) on finances.account_plan_id = pc."id"`)
+			.joinRaw('join patients on finances.client_id = patients."id"')
+			.joinRaw(
+				"join economic_groups on finances.economic_group_id = economic_groups.id",
+			)
+			.joinRaw("join systems on economic_groups.system_id = systems.id")
+			.joinRaw(
+				"join business_units on finances.business_unit_id = business_units.id",
+			)
+			.whereIn("finances.business_unit_id", [authCtx.unit.id])
+			.whereNull("finances.deleted_at")
+			.where("finances.competence_date", `0${4}/2024`)
+			.orderByRaw(
+				'finances."type", finances.issue_date, finances."document", finances.installment',
+			);
 		const workbook = XLSX.utils.book_new();
 		const worksheet = XLSX.utils.json_to_sheet([]);
 

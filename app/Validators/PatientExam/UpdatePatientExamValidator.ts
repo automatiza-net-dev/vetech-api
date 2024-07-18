244de40d75eb6c11ad54d17d78c439879271b844
STNeto1 <gfreitasneto18@gmail.com>
2024-07-18 09:38:24 -0300

    fix: validator

--- app/Validators/PatientExam/UpdatePatientExamValidator.ts
+++ app/Validators/PatientExam/UpdatePatientExamValidator.ts
@@ -1,27 +1,27 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator';
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { CustomMessages, rules, schema } from "@ioc:Adonis/Core/Validator";
 
 export default class UpdatePatientExamValidator {
-  constructor(protected ctx: HttpContextContract) {}
+	constructor(protected ctx: HttpContextContract) {}
 
-  public schema = schema.create({
-    laboratory: schema.string.optional(),
-    report: schema.string(),
-    patientId: schema.string({}, [
-      rules.exists({ table: 'patients', column: 'id' }),
-    ]),
-    scheduleId: schema.string.optional({}, [
-      rules.exists({ table: 'schedules', column: 'id' }),
-    ]),
-    executionerId: schema.string.optional({}, [
-      rules.exists({ table: 'users', column: 'id' }),
-    ]),
-    executedAt: schema.date.optional({}),
-    resultDate: schema.date.optional({}),
-    releasedAt: schema.date.optional({}),
-    realizedAt: schema.date.optional({}),
-    status: schema.string.optional({}),
-  });
+	public schema = schema.create({
+		laboratory: schema.string.optional(),
+		report: schema.string(),
+		patientId: schema.string({}, [
+			rules.exists({ table: "patients", column: "id" }),
+		]),
+		scheduleId: schema.string.optional({}, [
+			rules.exists({ table: "schedules", column: "id" }),
+		]),
+		executionerId: schema.string.optional({}, [
+			rules.exists({ table: "users", column: "id" }),
+		]),
+		executedAt: schema.date.optional({}),
+		resultDate: schema.date.optional({}),
+		releasedAt: schema.date.optional({}),
+		realizedAt: schema.date.optional({}),
+		status: schema.string.optional({}),
+	});
 
-  public messages: CustomMessages = {};
+	public messages: CustomMessages = {};
 }

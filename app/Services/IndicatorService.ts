233bc09f658b8ec4d3e38f8208b4b1f7ae8ff1c7
STNeto1 <gfreitasneto18@gmail.com>
2024-08-26 17:02:12 -0300

    fix: remove formatting

--- app/Services/IndicatorService.ts
+++ app/Services/IndicatorService.ts
@@ -7204,7 +7204,7 @@ export default class IndicatorService {
 					users: unitUsers.map((usr) => ({
 						userId: usr.id_avaliador,
 						userName: usr.nome_avaliador,
-						qtdClientes: this.shared.formatter.format(usr.qtd_clientes),
+						qtdClientes: usr.qtd_clientes,
 						valorRealizado: this.shared.formatter.format(usr.total_realizado),
 						ticketMedioRealizado: this.shared.formatter.format(
 							usr.tkt_medio_realizado,
@@ -7212,7 +7212,7 @@ export default class IndicatorService {
 						participacaoRealizado: this.shared.formatPercentage(
 							(usr.total_realizado / reviewedSum) * 100,
 						),
-						qtdAvaliacoes: this.shared.formatter.format(usr.qtd_avaliacoes),
+						qtdAvaliacoes: usr.qtd_avaliacoes,
 						totalAvaliado: this.shared.formatter.format(usr.total_avaliacoes),
 						ticketMedioAvaliacoes: this.shared.formatter.format(
 							usr.tkt_medio_avaliacoes,

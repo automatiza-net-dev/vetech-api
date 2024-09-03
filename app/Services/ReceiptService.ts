55f71f594e4f0071ad8b34f58001dc863dd4fc21
STNeto1 <gfreitasneto18@gmail.com>
2024-09-03 15:44:32 -0300

    fix: avg query

--- app/Services/ReceiptService.ts
+++ app/Services/ReceiptService.ts
@@ -2038,7 +2038,7 @@ export default class ReceiptService {
 			await Database.rawQuery(
 				`update deposit_items set quantity =
 (
-    select (di.quantity + sum(ri.quantity * ri.fraction_value))
+    select (avg(di.quantity) + sum(ri.quantity * ri.fraction_value))
     from deposit_items di
       join deposits d on di.deposit_id = d.id
       join receipt_items ri on ri.product_variation_id = di.product_variation_id and ri.business_unit_id = d.business_unit_id

415abdec1124a7b9bfb9eb8a54c3d63e903e1c1b
STNeto1 <gfreitasneto18@gmail.com>
2024-09-09 09:15:35 -0300

    fix: msg

--- app/Validators/UnavailableDay/CreateUnavailableDayValidator.ts
+++ app/Validators/UnavailableDay/CreateUnavailableDayValidator.ts
@@ -1,25 +1,34 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator';
-import WeekDay from 'App/Models/shared/WeekDay';
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { CustomMessages, rules, schema } from "@ioc:Adonis/Core/Validator";
+import WeekDay from "App/Models/shared/WeekDay";
 
 export default class CreateUnavailableDayValidator {
-  constructor(protected ctx: HttpContextContract) {}
+	constructor(protected ctx: HttpContextContract) {}
 
-  public schema = schema.create({
-    title: schema.string(),
-    userId: schema.string({}, [
-      rules.uuid(),
-      rules.exists({
-        table: 'users',
-        column: 'id',
-      }),
-    ]),
-    frequency: schema.array().members(schema.enum(Object.values(WeekDay), [])),
-    startDate: schema.date.optional({}),
-    endDate: schema.date.optional({}),
-    startHour: schema.string({}),
-    endHour: schema.string({}),
-  });
+	public schema = schema.create({
+		title: schema.string(),
+		userId: schema.string({}, [
+			rules.uuid(),
+			rules.exists({
+				table: "users",
+				column: "id",
+			}),
+		]),
+		frequency: schema.array().members(schema.enum(Object.values(WeekDay), [])),
+		startDate: schema.date.optional({}),
+		endDate: schema.date.optional({}),
+		startHour: schema.string({}),
+		endHour: schema.string({}),
+	});
 
-  public messages: CustomMessages = {};
+	public messages: CustomMessages = {
+		"startDate.date_format": "O campo de data de início deve ser um dia válido",
+		"endDate.date_format": "O campo de data de fim deve ser um dia válido",
+		"startHour.date_format":
+			"O campo de hora de início deve ser um horário válido",
+		"endHour.date_format": "O campo de hora de fim deve ser um horário válido",
+		"title.required": "O título é obrigatório e está faltando",
+		"startHour.required": "A hora de início é obrigatório e está faltando",
+		"endHour.required": "A hora de fim é obrigatório e está faltando",
+	};
 }

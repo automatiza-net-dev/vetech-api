e03742e934fe8f3d3e9920efb99d4728dcabfb90
STNeto1 <gfreitasneto18@gmail.com>
2024-09-16 10:19:31 -0300

    feat: add new indicators

--- app/Services/IndicatorService.ts
+++ app/Services/IndicatorService.ts
@@ -4054,6 +4054,10 @@ export default class IndicatorService {
 				() => authCtx.hasPermission("IND25"),
 				() => this.budgetsIndicators_2(authCtx, { ...data, type: "VENDEDOR" }),
 			),
+			SharedService.NoopPromise(
+				() => authCtx.hasPermission("IND27"),
+				() => this.consolidatedReviewerBudgets(authCtx, data),
+			),
 		]);
 
 		const cards = await Promise.all([
@@ -4092,6 +4096,10 @@ export default class IndicatorService {
 			SharedService.NoopPromise(
 				() => authCtx.hasPermission("IND17"),
 				() => this.installmentAvgIndicators(authCtx, data),
+			),
+			SharedService.NoopPromise(
+				() => authCtx.hasPermission("IND12"),
+				() => this.clientGroupTreeIndicators(authCtx, data),
 			),
 		]);
 

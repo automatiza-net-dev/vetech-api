553ae0b690348e0b994a7a4143ea4829a32117e6
STNeto1 <gfreitasneto18@gmail.com>
2024-10-04 09:45:26 -0300

    feat: meta unique description

--- app/Services/MetaService.ts
+++ app/Services/MetaService.ts
@@ -1,121 +1,137 @@
-import { inject } from '@adonisjs/fold';
-import Database from '@ioc:Adonis/Lucid/Database';
-import BadRequestException from 'App/Exceptions/BadRequestException';
-import Meta from 'App/Models/Meta';
-import SharedService, { AuthContext } from 'App/Services/SharedService';
+import { inject } from "@adonisjs/fold";
+import Database from "@ioc:Adonis/Lucid/Database";
+import BadRequestException from "App/Exceptions/BadRequestException";
+import Meta from "App/Models/Meta";
+import SharedService, { AuthContext } from "App/Services/SharedService";
 
 interface ISearch {
-  description?: string;
+	description?: string;
 }
 
 @inject()
 export default class MetaService {
-  constructor(private sharedService: SharedService) {}
+	constructor(private sharedService: SharedService) {}
 
-  async index(authCtx: AuthContext, data: ISearch) {
-    const qb = Meta.query()
-      .preload('group', query => {
-        query.select('id', 'company_name');
-      })
-      .where('system_id', authCtx.system.id)
-      .whereRaw('(economic_group_id = ? or economic_group_id is null)', [
-        authCtx.group.id,
-      ]);
+	async index(authCtx: AuthContext, data: ISearch) {
+		const qb = Meta.query()
+			.preload("group", (query) => {
+				query.select("id", "company_name");
+			})
+			.where("system_id", authCtx.system.id)
+			.whereRaw("(economic_group_id = ? or economic_group_id is null)", [
+				authCtx.group.id,
+			]);
 
-    if (data.description) {
-      qb.whereILike('description', `%${data.description}%`);
-    }
+		if (data.description) {
+			qb.whereILike("description", `%${data.description}%`);
+		}
 
-    return qb;
-  }
+		return qb;
+	}
 
-  async store(
-    authCtx: AuthContext,
-    data: {
-      description: string;
-      type: string;
-    },
-  ) {
-    await Database.transaction(async trx => {
-      const existing = await Meta.query()
-        .where('economic_group_id', authCtx.group.id)
-        .where('system_id', authCtx.system.id)
-        .whereRaw('lower(description) = lower(?)', [data.description]);
+	async store(
+		authCtx: AuthContext,
+		data: {
+			description: string;
+			type: string;
+		},
+	) {
+		await Database.transaction(async (trx) => {
+			const existing = await Meta.query()
+				.whereRaw("(economic_group_id = ? or economic_group_id is null)", [
+					authCtx.group.id,
+				])
+				.where("system_id", authCtx.system.id)
+				.whereRaw("lower(unaccent((description)) = lower(unaccent(?))", [
+					data.description,
+				]);
 
-      if (existing.length > 0) {
-        throw new BadRequestException('Meta já cadastrada', 400, 'E_ERR');
-      }
+			if (existing.length > 0) {
+				throw new BadRequestException(
+					"Meta já cadastrada com essa descrição já existe",
+					400,
+					"E_ERR",
+				);
+			}
 
-      await Meta.create(
-        {
-          economic_group_id: authCtx.group.id,
-          system_id: authCtx.system.id,
+			await Meta.create(
+				{
+					economic_group_id: authCtx.group.id,
+					system_id: authCtx.system.id,
 
-          description: data.description,
-          type: data.type,
-        },
-        {
-          client: trx,
-        },
-      );
-    });
-  }
+					description: data.description,
+					type: data.type,
+				},
+				{
+					client: trx,
+				},
+			);
+		});
+	}
 
-  async update(
-    authCtx: AuthContext,
-    id: string,
-    data: {
-      description: string;
-      type: string;
-      active: boolean;
-    },
-  ) {
-    await Database.transaction(async trx => {
-      const model = await Meta.query()
-        .useTransaction(trx)
-        .where('system_id', authCtx.system.id)
-        .where('economic_group_id', authCtx.group.id)
-        .where('id', id)
-        .first();
+	async update(
+		authCtx: AuthContext,
+		id: string,
+		data: {
+			description: string;
+			type: string;
+			active: boolean;
+		},
+	) {
+		await Database.transaction(async (trx) => {
+			const model = await Meta.query()
+				.useTransaction(trx)
+				.where("system_id", authCtx.system.id)
+				.where("economic_group_id", authCtx.group.id)
+				.where("id", id)
+				.first();
 
-      if (!model) {
-        throw this.sharedService.ResourceNotFound();
-      }
+			if (!model) {
+				throw this.sharedService.ResourceNotFound();
+			}
 
-      if (model.description !== data.description) {
-        const existing = await Meta.query()
-          .useTransaction(trx)
-          .where('system_id', authCtx.system.id)
-          .where('economic_group_id', authCtx.group.id)
-          .whereRaw('lower(description) = lower(?)', [data.description]);
+			if (model.description !== data.description) {
+				const existing = await Meta.query()
+					.useTransaction(trx)
+					.where("system_id", authCtx.system.id)
+					.whereRaw("(economic_group_id = ? or economic_group_id is null)", [
+						authCtx.group.id,
+					])
+					.whereRaw("lower(unaccent((description)) = lower(unaccent(?))", [
+						data.description,
+					]);
 
-        if (existing.length > 0) {
-          throw new BadRequestException('Meta já cadastrada', 400, 'E_ERR');
-        }
-      }
+				if (existing.length > 0) {
+					throw new BadRequestException(
+						"Meta já cadastrada com essa descrição já existe",
+						400,
+						"E_ERR",
+					);
+				}
+			}
 
-      return model
-        .merge({
-          description: data.description,
-          type: data.type,
-          active: data.active,
-        })
-        .useTransaction(trx)
-        .save();
-    });
-  }
+			return model
+				.merge({
+					description: data.description,
+					type: data.type,
+					active: data.active,
+				})
+				.useTransaction(trx)
+				.save();
+		});
+	}
 
-  async destroy(authCtx: AuthContext, id: string) {
-    const model = await Meta.query()
-      .where('system_id', authCtx.system.id)
-      .where('economic_group_id', authCtx.group.id)
-      .where('id', id)
-      .first();
+	async destroy(authCtx: AuthContext, id: string) {
+		const model = await Meta.query()
+			.where("system_id", authCtx.system.id)
+			.where("economic_group_id", authCtx.group.id)
+			.where("id", id)
+			.first();
 
-    if (!model) {
-      throw this.sharedService.ResourceNotFound();
-    }
+		if (!model) {
+			throw this.sharedService.ResourceNotFound();
+		}
 
-    await model.softDelete();
-  }
+		await model.softDelete();
+	}
 }

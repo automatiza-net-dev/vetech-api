e46c25d8cc43ee1ddace93ad241a7447cac9a28c
STNeto1 <gfreitasneto18@gmail.com>
2024-10-16 18:29:30 -0300

    feat: block pending budgets

--- app/Services/BudgetService.ts
+++ app/Services/BudgetService.ts
@@ -1554,13 +1554,13 @@ export default class BudgetService {
 			);
 		}
 
-		// if (model.pending) {
-		// 	throw new BadRequestException(
-		// 		"Este orçamento possui pendencias de Cortesia/Desconto Máximo que precisam ser aprovadas antes de ser confirmado",
-		// 		400,
-		// 		"E_ERR",
-		// 	);
-		// }
+		if (model.pending) {
+			throw new BadRequestException(
+				"Orçamento possui pendencias de liberação e não pode ser confirmado",
+				400,
+				"E_ERR",
+			);
+		}
 
 		const client = await Patient.query()
 			.where("id", model.client_id)

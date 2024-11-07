c34591e6bc729dce95f9ebd7159cf5b822971022
STNeto1 <gfreitasneto18@gmail.com>
2024-11-07 14:43:10 -0300

    fix: maybe

--- app/Services/ScheduleService.ts
+++ app/Services/ScheduleService.ts
@@ -1229,7 +1229,7 @@ export default class ScheduleService {
 					id: string;
 					description: string;
 				} | null;
-				attendaces: {
+				attendances: {
 					id: number;
 					scheduleService: {
 						id: string;
@@ -1239,7 +1239,17 @@ export default class ScheduleService {
 				patient: {
 					id: string;
 					name: string;
-					tutor: { cellphone: string | null; telephone: string | null };
+					photo: string | null;
+					tag: string;
+					cellphone: string | null;
+				} | null;
+				race: {
+					id: string;
+					description: string;
+				} | null;
+				specie: {
+					id: string;
+					description: string;
 				} | null;
 			}[],
 			WorkingDay[],
@@ -1319,7 +1329,37 @@ export default class ScheduleService {
 			.joinRaw("left join races rc on pa.race_id = rc.id")
 			.joinRaw("left join species sp on rc.specie_id = sp.id");
 
-		resultData[0] = await schedulesQb;
+		// resultData[0] = await schedulesQb;
+		resultData[0] = [
+			{
+				id: "54deb689-3e8b-40a1-a351-70d4a64d2ba6",
+				user_id: "14fb9560-1c96-4883-b73b-0623b0e0cb34",
+				start_hour: "2024-10-29T08:00:00.000Z",
+				end_hour: "2024-10-29T08:44:00.000Z",
+				service_type: {
+					id: "13ff866b-59d3-41b6-a6b2-54c2ca2ff612",
+					description: "Consulta Odontológica",
+					type: "A",
+				},
+				service_status: {
+					id: "06f9836f-75a3-4dfa-a1bc-578aa60305ad",
+					description: "Faltou",
+					color: "#FF7F27",
+					type: "FAL",
+				},
+				reason: null,
+				attendances: [],
+				patient: {
+					id: "2823d94c-a11d-4e50-bd9b-7d1dbf8a8656",
+					name: "LARISSA RODRIGUES DA LUZ DE OLIVEIRA",
+					photo: null,
+					tag: "9011",
+					cellphone: null,
+				},
+				race: null,
+				specie: null,
+			},
+		];
 
 		if (typeof data.working === "undefined" || data.working === "true") {
 			resultData[1] = await WorkingDay.query()
@@ -1422,9 +1462,9 @@ export default class ScheduleService {
 						)
 						.map((day) => ({
 							//@ts-ignore
-							start: day.startHour,
+							start: day.start_hour,
 							//@ts-ignore
-							end: day.endHour,
+							end: day.start_hour,
 							event: day,
 							name: elem.name,
 							type: this.getEventLabel(day),

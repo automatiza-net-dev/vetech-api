fdb5f6169950f373673828945ff65e3891c43b69
STNeto1 <gfreitasneto18@gmail.com>
2024-12-20 09:36:25 -0300

    fix: mapping

--- app/Services/ReportService.ts
+++ app/Services/ReportService.ts
@@ -3141,7 +3141,7 @@ left join crm_statuses cs on opportunities.status_id = cs.id) on marketing_campa
 										total: contas.reduce((acc, curr) => acc + curr.total, 0),
 										refCusto: accountPlanChildren
 											.filter((apc) => apc.parent_id === ap.id)
-											.map((c) => c.ref),
+											.map((c) => c.id),
 										refs: contas.map((c) => c.id),
 										itens: contas,
 									};

8413000f3e1ed94a9fd644075ceaa34c2103214e
STNeto1 <gfreitasneto18@gmail.com>
2024-12-20 14:55:08 -0300

    fix: remove field

--- app/Services/ReportService.ts
+++ app/Services/ReportService.ts
@@ -3129,7 +3129,7 @@ left join crm_statuses cs on opportunities.status_id = cs.id) on marketing_campa
 										.filter((apc) => apc.parent_id === ap.id)
 										.map((apc) => ({
 											id: apc.id,
-											ref: apc.ref,
+											// ref: apc.ref,
 											tag: apc.tag,
 											basear: false,
 											description: apc.description,

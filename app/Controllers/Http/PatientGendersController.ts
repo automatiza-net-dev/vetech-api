ee0a7a725e812e50e1caad82fff732104b87e016
STNeto1 <gfreitasneto18@gmail.com>
2025-01-23 10:16:19 -0300

    feat: patient genders http

--- app/Controllers/Http/PatientGendersController.ts
+++ app/Controllers/Http/PatientGendersController.ts
@@ -1,3 +1,21 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { inject } from "@adonisjs/fold";
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import PatientGenderService from "App/Services/PatientGenderService";
+import SharedService from "App/Services/SharedService";
 
-export default class PatientGendersController {}
+@inject()
+export default class PatientGendersController {
+	constructor(
+		private sharedService: SharedService,
+		private service: PatientGenderService,
+	) {}
+
+	public async list({ request, response, auth }: HttpContextContract) {
+		const result = await this.service.distinctGenders(
+			await this.sharedService.getAuthContext(auth),
+			request.qs(),
+		);
+
+		return response.ok(result);
+	}
+}

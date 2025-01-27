d5f9ccb30d1ab299d85f894d3fa7c43b0deecb94
STNeto1 <gfreitasneto18@gmail.com>
2025-01-27 11:20:51 -0300

    fix: resume

--- app/Validators/HospitalizationMedicalPrescription/CreateHospitalizationMedicalPrescriptionValidator.ts
+++ app/Validators/HospitalizationMedicalPrescription/CreateHospitalizationMedicalPrescriptionValidator.ts
@@ -1,30 +1,30 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator';
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { CustomMessages, rules, schema } from "@ioc:Adonis/Core/Validator";
 import {
-  MedicalPrescriptionFrequency,
-  MedicalPrescriptionType,
-} from 'App/Models/MedicalPrescription';
+	MedicalPrescriptionFrequency,
+	MedicalPrescriptionType,
+} from "App/Models/MedicalPrescription";
 
 export default class CreateHospitalizationMedicalPrescriptionValidator {
-  constructor(protected ctx: HttpContextContract) {}
+	constructor(protected ctx: HttpContextContract) {}
 
-  public schema = schema.create({
-    hospitalizationId: schema.string({}, [
-      rules.uuid(),
-      rules.exists({ table: 'hospitalizations', column: 'id' }),
-    ]),
-    userId: schema.string.optional({}, [
-      rules.uuid(),
-      rules.exists({ table: 'users', column: 'id' }),
-    ]),
-    type: schema.enum(Object.values(MedicalPrescriptionType)),
-    prescribedAt: schema.date(),
-    executionStart: schema.date(),
-    frequency: schema.enum(Object.values(MedicalPrescriptionFrequency)),
-    description: schema.string(),
-    resume: schema.string(),
-    volume: schema.string.optional(),
-  });
+	public schema = schema.create({
+		hospitalizationId: schema.string({}, [
+			rules.uuid(),
+			rules.exists({ table: "hospitalizations", column: "id" }),
+		]),
+		userId: schema.string.optional({}, [
+			rules.uuid(),
+			rules.exists({ table: "users", column: "id" }),
+		]),
+		type: schema.enum(Object.values(MedicalPrescriptionType)),
+		prescribedAt: schema.date(),
+		executionStart: schema.date(),
+		frequency: schema.enum(Object.values(MedicalPrescriptionFrequency)),
+		description: schema.string(),
+		resume: schema.string.optional(),
+		volume: schema.string.optional(),
+	});
 
-  public messages: CustomMessages = {};
+	public messages: CustomMessages = {};
 }

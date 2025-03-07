5170e93804ab91552bf41fc787a2172bc69d64e5
STNeto1 <gfreitasneto18@gmail.com>
2025-03-07 09:53:25 -0300

    feat: forgot password

--- app/Validators/User/ForgotPasswordValidator.ts
+++ app/Validators/User/ForgotPasswordValidator.ts
@@ -1,16 +1,14 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator';
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { CustomMessages, rules, schema } from "@ioc:Adonis/Core/Validator";
 
 export default class ForgotPasswordValidator {
-  constructor(protected ctx: HttpContextContract) {}
-  public schema = schema.create({
-    email: schema.string({}, [
-      rules.email(),
-      rules.exists({ table: 'users', column: 'email' }),
-    ]),
-    systemName: schema.string({}, [
-      rules.exists({ table: 'systems', column: 'name' }),
-    ]),
-  });
-  public messages: CustomMessages = {};
+	constructor(protected ctx: HttpContextContract) {}
+	public schema = schema.create({
+		systemId: schema.number([rules.exists({ table: "systems", column: "id" })]),
+		email: schema.string({}, [
+			rules.email(),
+			rules.exists({ table: "users", column: "email" }),
+		]),
+	});
+	public messages: CustomMessages = {};
 }

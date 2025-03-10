1964fcd898ecabd48330964aa92c1ec2c3e929f3
STNeto1 <gfreitasneto18@gmail.com>
2025-03-10 18:28:23 -0300

    fix: sql 3

--- app/Services/SystemUrlService.ts
+++ app/Services/SystemUrlService.ts
@@ -10,7 +10,7 @@ export default class SystemUrlService {
 					"systems.id, systems.name, systems.type, systems.colors, system_urls.url, system_urls.primary_color, system_urls.secondary_color, system_urls.home_image_url, system_urls.logo_url",
 				),
 			)
-			.joinRaw("join system_urls su on systems.id = su.system_id")
+			.joinRaw("join system_urls on systems.id = system_urls.system_id")
 			.whereRaw("systems.active is true")
 			.whereRaw("system_urls.active is true")
 			.whereRaw("system_urls.url = ?", [data.url])

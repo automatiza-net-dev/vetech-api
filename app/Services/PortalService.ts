2348b0947037fcedf3d908899c0aa6b472bc9943
STNeto1 <germano@stneto.dev>
2025-03-12 11:21:25 -0300

    fix: sql

--- app/Services/PortalService.ts
+++ app/Services/PortalService.ts
@@ -1021,7 +1021,7 @@ sum(bill_items.total_value) as total, count(distinct bills.client_id) as clients
 			.select(
 				Database.raw(
 					`'Recorrentes'          as description,
-            sum(bills.total_value) as total,
+            coalesce(sum(bills.total_value), 0) as total,
             count(distinct bills.client_id) as qty_clients
           `,
 				),
@@ -1056,7 +1056,7 @@ sum(bill_items.total_value) as total, count(distinct bills.client_id) as clients
 						Database.raw(
 							`
 			       client_origins.description,
-			       sum(bills.total_value) as total,
+			       coalesce(sum(bills.total_value), 0) as total,
 			       count( distinct bills.client_id ) as qty_clients
 			       `,
 						),

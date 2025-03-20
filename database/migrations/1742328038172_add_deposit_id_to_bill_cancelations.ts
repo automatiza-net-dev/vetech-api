80a48649383936fab59e9217cfc951cb1f22405e
STNeto1 <gfreitasneto18@gmail.com>
2025-03-20 10:32:44 -0300

    fix: old migration

--- database/migrations/1742328038172_add_deposit_id_to_bill_cancelations.ts
+++ database/migrations/1742328038172_add_deposit_id_to_bill_cancelations.ts
@@ -1,7 +1,7 @@
 import BaseSchema from "@ioc:Adonis/Lucid/Schema";
 
 export default class extends BaseSchema {
-	protected tableName = "bill_cancelation";
+	protected tableName = "bill_cancelations";
 
 	public async up() {
 		this.schema.alterTable(this.tableName, (table) => {

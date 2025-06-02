6ec81fd846aa89a713bea8d9f5405cc152009110
STNeto1 <gfreitasneto18@gmail.com>
2025-06-02 09:02:39 -0300

    fix: realized

--- app/Validators/PatientExam/CreatePatientExamAttachmentValidator.ts
+++ app/Validators/PatientExam/CreatePatientExamAttachmentValidator.ts
@@ -1,16 +1,16 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator';
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { CustomMessages, rules, schema } from "@ioc:Adonis/Core/Validator";
 
 export default class CreatePatientExamAttachmentValidator {
-  constructor(protected ctx: HttpContextContract) {}
+	constructor(protected ctx: HttpContextContract) {}
 
-  public schema = schema.create({
-    patientId: schema.string({}, [
-      rules.exists({ table: 'patients', column: 'id' }),
-    ]),
-    realizedAt: schema.date(),
-    attachments: schema.array().members(schema.file()),
-  });
+	public schema = schema.create({
+		patientId: schema.string({}, [
+			rules.exists({ table: "patients", column: "id" }),
+		]),
+		realizedAt: schema.date.optional(),
+		attachments: schema.array().members(schema.file()),
+	});
 
-  public messages: CustomMessages = {};
+	public messages: CustomMessages = {};
 }

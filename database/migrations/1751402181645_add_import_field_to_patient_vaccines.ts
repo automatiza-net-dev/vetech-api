464bfab45a6f9b2f54edfe48b4c230b3a49298ff
STNeto1 <gfreitasneto18@gmail.com>
2025-07-01 17:52:06 -0300

    fix: import serialization

---
+++ database/migrations/1751402181645_add_import_field_to_patient_vaccines.ts
@@ -0,0 +1,17 @@
+import BaseSchema from "@ioc:Adonis/Lucid/Schema";
+
+export default class extends BaseSchema {
+	protected tableName = "patient_vaccines";
+
+	public async up() {
+		this.schema.alterTable(this.tableName, (table) => {
+			table.text("import_field").defaultTo("");
+		});
+	}
+
+	public async down() {
+		this.schema.alterTable(this.tableName, (table) => {
+			table.dropColumn("import_field");
+		});
+	}
+}

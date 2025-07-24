098a4a37a1dfb534dbc49b58b0ae0a7abc1f4312
STNeto1 <gfreitasneto18@gmail.com>
2025-07-24 09:30:25 -0300

    fix: logging

--- start/events.ts
+++ start/events.ts
@@ -13,37 +13,23 @@ import Logger from "@ioc:Adonis/Core/Logger";
 import { axiom } from "App/Lib/Axiom";
 import Env from "@ioc:Adonis/Core/Env";
 
-Event.on("db:query", async ({ sql, bindings, connection, method, model }) => {
-	if (!bindings) {
-		Logger.info("[SQL] %s", sql);
-		axiom.ingest(Env.get("AXIOM_DATASET"), [
-			{
-				_type: "sql",
-				sql,
-				connection,
-				method,
-				model: model ?? null,
-			},
-		]);
-		await axiom.flush();
-		return;
-	}
+const shouldLog = false;
 
-	Logger.info(
-		"[SQL] %s",
+Event.on("db:query", async ({ sql, bindings, connection, method, model }) => {
+	const _sql =
 		bindings?.reduce(
 			(currSql: string, binding) => currSql.replace("?", `'${binding}'`),
 			sql,
-		) ?? sql,
-	);
+		) ?? sql;
+
+	if (shouldLog) {
+		Logger.info("[SQL] %s", _sql);
+	}
+
 	axiom.ingest(Env.get("AXIOM_DATASET"), [
 		{
 			_type: "sql",
-			sql:
-				bindings?.reduce(
-					(currSql: string, binding) => currSql.replace("?", `'${binding}'`),
-					sql,
-				) ?? sql,
+			sql: _sql,
 			connection,
 			method,
 			model: model ?? null,

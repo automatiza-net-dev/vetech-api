dc59ce4ab977c9e5ed150a43ed822aa8033ee3d0
STNeto1 <gfreitasneto18@gmail.com>
2026-01-09 11:24:54 -0300

    feat: billing

--- app/Services/BusinessUnitFiscalDocumentService.ts
+++ app/Services/BusinessUnitFiscalDocumentService.ts
@@ -741,9 +741,9 @@ export default class BusinessUnitFiscalDocumentService {
         query.preload("product");
       });
 
-    if (items.length === 0) {
-      throw new BadRequestException("Não existe documento para ser emitido");
-    }
+    // if (items.length === 0) {
+    //   throw new BadRequestException("Não existe documento para ser emitido");
+    // }
 
     const productsWithoutServiceCode = items.filter((i) => !i.productVariation.product.serviceCode);
     if (productsWithoutServiceCode.length > 0) {

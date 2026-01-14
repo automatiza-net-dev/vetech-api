1401c3151d80174b37d73cfb6af544fa088e2e8e
STNeto1 <gfreitasneto18@gmail.com>
2026-01-14 09:07:42 -0300

    feat: nbs 2

--- app/Validators/Service/UpdateServiceValidator.ts
+++ app/Validators/Service/UpdateServiceValidator.ts
@@ -3,38 +3,39 @@ import { CustomMessages, rules, schema } from "@ioc:Adonis/Core/Validator";
 import { PServiceType } from "App/Models/Product";
 
 export default class UpdateServiceValidator {
-	constructor(protected ctx: HttpContextContract) {}
+  constructor(protected ctx: HttpContextContract) { }
 
-	public schema = schema.create({
-		description: schema.string({}, []),
-		referenceCode: schema.string.optional({}, []),
-		courtesy: schema.boolean.optional([]),
-		subgroupId: schema.string({}, [
-			rules.uuid(),
-			rules.exists({
-				table: "subgroups",
-				column: "id",
-			}),
-		]),
-		features: schema.string.optional({}, []),
-		taxationGroupId: schema.string({}, [
-			rules.uuid(),
-			rules.exists({
-				table: "taxation_groups",
-				column: "id",
-			}),
-		]),
-		unitId: schema.string.optional({}, [
-			rules.uuid(),
-			rules.exists({
-				table: "units",
-				column: "id",
-			}),
-		]),
-		serviceCode: schema.string.optional(),
-		active: schema.boolean(),
-		serviceType: schema.enum(PServiceType),
-	});
+  public schema = schema.create({
+    description: schema.string({}, []),
+    referenceCode: schema.string.optional({}, []),
+    courtesy: schema.boolean.optional([]),
+    subgroupId: schema.string({}, [
+      rules.uuid(),
+      rules.exists({
+        table: "subgroups",
+        column: "id",
+      }),
+    ]),
+    features: schema.string.optional({}, []),
+    taxationGroupId: schema.string({}, [
+      rules.uuid(),
+      rules.exists({
+        table: "taxation_groups",
+        column: "id",
+      }),
+    ]),
+    unitId: schema.string.optional({}, [
+      rules.uuid(),
+      rules.exists({
+        table: "units",
+        column: "id",
+      }),
+    ]),
+    serviceCode: schema.string.optional(),
+    codigoNbs: schema.string.optional(),
+    active: schema.boolean(),
+    serviceType: schema.enum(PServiceType),
+  });
 
-	public messages: CustomMessages = {};
+  public messages: CustomMessages = {};
 }
--- contracts/interfaces/IServiceData.ts
+++ contracts/interfaces/IServiceData.ts
@@ -2,46 +2,48 @@ import { BusinessUnitProductMetaType } from "App/Models/BusinessUnitProduct";
 import { PServiceType } from "App/Models/Product";
 
 interface IPrice {
-	price: number;
-	profitMargin?: number;
-	costPrice?: number;
-	maximumDiscountPercentage?: number;
-	maximumDiscountValue?: number;
-	commission?: number;
-	meta?: number;
-	metaType?: BusinessUnitProductMetaType;
-	commissionMeta?: number;
+  price: number;
+  profitMargin?: number;
+  costPrice?: number;
+  maximumDiscountPercentage?: number;
+  maximumDiscountValue?: number;
+  commission?: number;
+  meta?: number;
+  metaType?: BusinessUnitProductMetaType;
+  commissionMeta?: number;
 }
 
 export default interface IServiceData {
-	description: string;
-	referenceCode?: string;
-	subgroupId: string;
+  description: string;
+  referenceCode?: string;
+  subgroupId: string;
 
-	features?: string;
-	serviceCode?: string;
-	courtesy?: boolean;
-	productivityItem?: boolean;
+  features?: string;
+  serviceCode?: string;
+  codigoNbs?: number;
+  courtesy?: boolean;
+  productivityItem?: boolean;
 
-	taxationGroupId: string;
-	unitId?: string;
-	serviceType?: (typeof PServiceType)[number];
+  taxationGroupId: string;
+  unitId?: string;
+  serviceType?: (typeof PServiceType)[number];
 
-	price: IPrice;
+  price: IPrice;
 }
 
 export interface IUpdateService {
-	description: string;
-	referenceCode?: string;
-	subgroupId: string;
-	serviceCode?: string;
-	active: boolean;
+  description: string;
+  referenceCode?: string;
+  subgroupId: string;
+  serviceCode?: string;
+  codigoNbs?: number;
+  active: boolean;
 
-	productivityItem?: boolean;
-	courtesy?: boolean;
+  productivityItem?: boolean;
+  courtesy?: boolean;
 
-	features?: string;
-	taxationGroupId: string;
-	unitId?: string;
-	serviceType?: (typeof PServiceType)[number];
+  features?: string;
+  taxationGroupId: string;
+  unitId?: string;
+  serviceType?: (typeof PServiceType)[number];
 }
